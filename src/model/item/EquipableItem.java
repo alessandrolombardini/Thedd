fbdac3e2a84923a1facc7e1903715d574421e7c8
Alex Speranza <alex.speranza@studio.unibo.it>
2019-02-13 18:17:01 +0100

    Add type getter

--- src/model/item/EquipableItem.java
+++ src/model/item/EquipableItem.java
@@ -15,4 +15,11 @@ public interface EquipableItem extends Item {
      *  the map containing the statistics and the modifier granted by the item
      */
     Map<Statistic, Integer> getModifiers();
+
+    /**
+     * 
+     * @return
+     *  the type of the item
+     */
+    EquipableItemType getType();
 }
