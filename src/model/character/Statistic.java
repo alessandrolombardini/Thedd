8d78b9957151d99c0becb7bced2662b326315c3a
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2019-02-16 16:07:52 +0100

    Add Statistic enum

---
+++ src/model/character/Statistic.java
@@ -0,0 +1,19 @@
+package model.character;
+
+/**
+ * Representation of the statistics of game's characters.
+ */
+public enum Statistic {
+
+    /** Punti Vita. */
+    PV,
+
+    /** Costituzione. */
+    COS,
+
+    /** Forza. */
+    FOR,
+
+    /** Riflessi. */
+    RIFL
+}
