ca6122779f28eaa203662a53f05519c37641763b
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-02-19 21:55:48 +0100

    Added setNextTarget method

--- combat/interfaces/AutomaticActionActor.java
+++ combat/interfaces/AutomaticActionActor.java
@@ -1,7 +1,11 @@
 package combat.interfaces;
 
+import java.util.List;
+
 public interface AutomaticActionActor extends ActionActor {
  
     void setNextAction();
+
+    void setNextTarget(List<ActionActor> availableTargets);
 
 }
