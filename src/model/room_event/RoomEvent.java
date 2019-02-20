a96349ce6fa867c01647c5641999d328b820ba33
Alex Speranza <alex.speranza@studio.unibo.it>
2019-02-20 18:46:28 +0100

    Add RoomEvent interface to model a generic event

---
+++ src/model/room_event/RoomEvent.java
@@ -0,0 +1,13 @@
+package model.room_event;
+
+/**
+ * Generic event inside a room. It has a {@link model.room_event.RoomEventType}.
+ *
+ */
+public interface RoomEvent {
+    /**
+     * 
+     * @return the type of an event inside the room
+     */
+    RoomEventType getType();
+}
