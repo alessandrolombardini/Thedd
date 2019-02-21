39335d182d279b729cad67150ff23b08451ba4ba
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-02-21 10:59:52 +0100

    Modify accordingly to changes to the interface

--- combat/implementations/AbstractNPCAction.java
+++ combat/implementations/AbstractNPCAction.java
@@ -6,32 +6,22 @@ import combat.interfaces.NPCAction;
 
 public abstract class AbstractNPCAction extends AbstractAction implements NPCAction {
 
-    private double lowerBound;
-    private double upperBound = 100;
+    private double weight;
 
-    public AbstractNPCAction(final ActionActor source, final String name, final double lowerBound, final double upperBound, final TargetType targetType, final double baseHitChance) {
+    public AbstractNPCAction(final ActionActor source, final String name, final double weight, final TargetType targetType, final double baseHitChance) {
         super(source, name, baseHitChance, targetType);
-        setPickChanceBounds(lowerBound, upperBound);
+        setPickWeight(weight);
         setTargetType(targetType);
     }
 
     @Override
-    public void setPickChanceBounds(final double lowerBound, final double upperBound) {
-        if (lowerBound > upperBound) {
-            throw new IllegalArgumentException("Inserted lower bound cannot be higher than upper bound");
-        }
-        this.lowerBound = lowerBound < 0 ? 0 : lowerBound > 1 ? 1 : lowerBound;
-        this.upperBound = upperBound < 0 ? 0 : upperBound > 1 ? 1 : upperBound;
+    public void setPickWeight(final double weight) {
+        this.weight = weight;
     }
 
     @Override
-    public double getPickChanceLowerBound() {
-        return lowerBound;
-    }
-
-    @Override
-    public double getPickChanceUpperBound() {
-        return upperBound;
+    public double getPickWeight() {
+        return weight;
     }
 
 }
