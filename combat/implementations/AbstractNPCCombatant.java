7966725e50af33749a997c4b636ab97503234232
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-02-21 19:12:31 +0100

    AbstractNPCCombatant: updated to make use of new RandomCollection interface and to implement changes in AutomaticActionActor interface

--- combat/implementations/AbstractNPCCombatant.java
+++ combat/implementations/AbstractNPCCombatant.java
@@ -2,57 +2,33 @@ package combat.implementations;
 
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 import combat.interfaces.Action;
 import combat.interfaces.ActionActor;
 import combat.interfaces.AutomaticActionActor;
-import combat.interfaces.NPCAction;
+import utils.RandomCollection;
 
 public abstract class AbstractNPCCombatant extends AbstractCombatant implements AutomaticActionActor {
-
-    private List<NPCAction> actionList;
 
     public AbstractNPCCombatant(final String name) {
         super(name);
     }
 
     @Override
-    public void setAvailableActionsList(final List<? extends Action> actions) {
-        super.setAvailableActionsList(actions);
-        actionList = getAvailableActionsList()
-                                    .stream()
-                                    .map(a -> (NPCAction) a)
-                                    .collect(Collectors.toList());
-        final double totalActionsProbabilities = actionList.stream()
-                .mapToDouble(a -> a.getPickWeight())
-                .sum();
-        if (totalActionsProbabilities != 1.0) {
-            throw new IllegalStateException("Actions pick chance's upper bounds do not amount to 1");
-        }
+    public RandomCollection<Action> addWeightedAction(final Action action, final double weight) {
+        return getWeightedActions().add(action, weight);
+    }
+
+    @Override
+    public
+    RandomCollection<Action> updateActionWeight(final Action action, final double newWeight) {
+        getWeightedActions().updateItemWeight(action, newWeight);
+        return getWeightedActions();
     }
 
     @Override
     public void setNextAction() {
-        //Code taken from https://stackoverflow.com/questions/6737283/weighted-randomness-in-java
-        // Compute the total weight of all items together
-        double random = 0d;
-        double totalWeight = 0.0d;
-        for (final NPCAction a : actionList) {
-            totalWeight += a.getPickWeight();
-        }
-
-        // Now choose a random item
-        while (random == 0d) {
-            random = Math.random() * totalWeight;
-        }
-        for (final NPCAction a : actionList) {
-            random -= a.getPickWeight();
-            if (random <= 0d) {
-                setAction(a);
-                break;
-            }
-        }
+        setAction(getWeightedActions().getNext());
     }
 
     @Override
