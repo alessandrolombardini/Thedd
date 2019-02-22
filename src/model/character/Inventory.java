2f9f6c2fd5e1e6d7a9b9ef952ab394309cefb28d
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2019-02-22 18:03:47 +0100

    Add getEquipableItem(), getUsableItem() and remove() methods

--- src/model/character/Inventory.java
+++ src/model/character/Inventory.java
@@ -2,7 +2,9 @@ package model.character;
 
 import java.util.Optional;
 
+import model.item.EquipableItem;
 import model.item.Item;
+import model.item.UsableItem;
 
 /**
  * Interface that manage a collection of items.
@@ -10,17 +12,32 @@ import model.item.Item;
 public interface Inventory {
 
     /**
-     * This method returns the specified item of the Inventory.
+     * This method returns the specified equipable item from the Inventory.
      * 
      * @param id the id of the object
-     * @return the specified item or null if is not present
+     * @return the equipable item or null if is not present or is not equipable
      */
-    Optional<Item> getItem(int id);
+    Optional<EquipableItem> getEquipableItem(int id);
+
+    /**
+     * This method returns the specified usable item of the Inventory.
+     * 
+     * @param id the id of the object
+     * @return the usable item or null if is not present or is not usable
+     */
+    Optional<UsableItem> getUsableItem(int id);
 
     /**
      * This method add the passed item in the Inventory.
+     * 
      * @param item the item to add
      */
     void addItem(Item item);
 
+    /**
+     * This method remove the specified id item from the Inventory.
+     * 
+     * @param id the id of the Item that is going to be removed
+     */
+    void removeItem(int id);
 }
