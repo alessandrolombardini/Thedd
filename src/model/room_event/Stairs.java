8a3cffa5c7adaf0042159829cb7b576a2157d562
Alex Speranza <alex.speranza@studio.unibo.it>
2019-02-25 19:09:09 +0100

    Add extension of AbstractRoomEvent and change constructor accordingly

--- src/model/room_event/Stairs.java
+++ src/model/room_event/Stairs.java
@@ -4,7 +4,16 @@ package model.room_event;
  * Implementation of Stairs type of {@link model.room_event.RoomEvent}.
  *
  */
-public final class Stairs implements RoomEvent {
+public final class Stairs extends AbstractRoomEvent implements RoomEvent {
+
+    private static final String NAME = "Stairs";
+
+    /**
+     * Create an instance of Stairs.
+     */
+    public Stairs() {
+        super(NAME);
+    }
 
     @Override
     public RoomEventType getType() {
