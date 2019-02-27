98910a6a6a84469f0bf36d458ab03395ed0d70b8
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2019-02-27 11:58:07 +0100

    Add an Interface for StatValues class

--- src/model/character/StatValuesImpl.java
+++ src/model/character/StatValuesImpl.java
@@ -3,7 +3,7 @@ package model.character;
 /**
  * This class represents statistics' pair of current and max value.
  */
-public class StatValues {
+public class StatValuesImpl implements StatValues {
 
     private int actual;
     private int max;
@@ -14,48 +14,31 @@ public class StatValues {
      * @param value specifies the max value of this statistic. The actual value is
      *              automatically set.
      */
-    public StatValues(final int value) {
+    public StatValuesImpl(final int value) {
         this.max = value;
         this.actual = value;
     }
 
-    /**
-     * This method update the actual field.
-     * 
-     * @param value is the value added at the actual field.
-     */
-    public void updateActual(final int value) {
+    @Override
+    public final void updateActual(final int value) {
         this.actual = this.actual + value;
     }
 
-    /**
-     * This method update the max field. The actual field is updated proportionally
-     * to the max one automatically.
-     * 
-     * @param value is the value added to the max field.
-     */
-    public void updateMax(final int value) {
+    @Override
+    public final void updateMax(final int value) {
         final int oldMax = this.max;
 
         this.max = this.max + value;
         this.actual = (int) (this.actual * this.max) / oldMax;
     }
 
-    /**
-     * Getter for actual value.
-     * 
-     * @return the actual value.
-     */
-    public int getActual() {
+    @Override
+    public final int getActual() {
         return this.actual;
     }
 
-    /**
-     * Getter for max value.
-     * 
-     * @return the max value.
-     */
-    public int getMax() {
+    @Override
+    public final int getMax() {
         return this.max;
     }
 }
