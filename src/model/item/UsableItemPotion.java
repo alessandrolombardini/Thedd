5e5c7ea7a87a781b81ab41d7e5ee47a0d91f08c6
Alex Speranza <alex.speranza@studio.unibo.it>
2019-02-27 12:25:46 +0100

    Add new usable item Potion

---
+++ src/model/item/UsableItemPotion.java
@@ -0,0 +1,22 @@
+package model.item;
+
+import java.util.ArrayList;
+/**
+ * Class of Item Potion that heals the target.
+ *
+ */
+public class UsableItemPotion extends UsableItemImpl {
+
+    private static final int ID = 0;
+    private static final String NAME = "Potion";
+    private static final String DESCRIPTION = "A mystical beverage that heal wounds";
+    private static final double HEALING_VALUE = 20.0;
+    /**
+     * Create an instance of a Potion item.
+     */
+    public UsableItemPotion() {
+        super(ID, NAME, new ArrayList<>(), DESCRIPTION);
+        this.addActionEffect(new HealingEffect(HEALING_VALUE));
+    }
+
+}
