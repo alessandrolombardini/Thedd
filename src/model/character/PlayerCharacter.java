e999026fe8d08627071fb993c64ac2a47f80a777
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2019-02-28 23:23:27 +0100

    Refactor the PlayerCharacter's static factory

--- src/model/character/PlayerCharacter.java
+++ src/model/character/PlayerCharacter.java
@@ -1,26 +1,19 @@
 package model.character;
 
-import java.util.ArrayList;
 import java.util.EnumMap;
-import java.util.List;
-
-import model.combat.enums.RandomActionPrority;
-import model.combat.implementations.ActionImpl;
-import model.combat.interfaces.Action;
 
 /**
  * Class that Implements a Player Character.
  */
 public class PlayerCharacter extends AbstractCharacter {
     // Questi valori sono messi a caso e credo non abbiano assolutamente senso.
-    // Datemi dei consigli su come farli meglio thankyouuuu
     private static final int basicPV = 50;
     private static final int basicCOS = 30;
     private static final int basicFOR = 30;
     private static final int basicRIFL = 20;
 
     private static final EnumMap<Statistic, StatValues> basicStat = new EnumMap<Statistic, StatValues>(Statistic.class);
-    private static final List<Action> basicActions = new ArrayList<>();
+
     static {
         basicStat.put(Statistic.PV, new StatValuesImpl(basicPV));
         basicStat.put(Statistic.COS, new StatValuesImpl(basicCOS));
@@ -40,14 +33,13 @@ public class PlayerCharacter extends AbstractCharacter {
     /**
      * PlayerCharacter's static factory.
      * 
-     * @param name       the name chosen by the player.
-     * @param multiplier it's the rate multiplied at the basic statistics.
-     * @return
+     * @param name the name chosen by the player.
+     * @return a new instance of Player Character
      */
     public static BasicCharacter createPlayerCharacter(final String name) {
         final BasicCharacter ret = new PlayerCharacter(name);
         ret.setBasicStat(basicStat);
-        ret.addWeightedAction(new ActionImpl()/*Devo ancora capire bene*/ , RandomActionPrority.DEFAULT);
+        //ret.addWeightedAction(new ActionImpl(), RandomActionPrority.DEFAULT);
         return ret;
     }
 
