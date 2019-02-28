d4854e6232e4decffc1b02e4e89bd98aac82c195
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2019-02-28 23:43:50 +0100

    Add GoblinNPC's static factory

--- src/model/character/GoblinNPC.java
+++ src/model/character/GoblinNPC.java
@@ -5,15 +5,40 @@ import java.util.EnumMap;
 /**
  * Class that implements a Goblin Non-Player Character.
  */
-public class GoblinNPC extends AbstractCharacter {
+public final class GoblinNPC extends AbstractCharacter {
+
+ // Questi valori sono messi a caso e credo non abbiano assolutamente senso.
+    private static final int BASIC_PV = 50;
+    private static final int BASIC_COS = 30;
+    private static final int BASIC_FOR = 30;
+    private static final int BASIC_RIFL = 20;
+
+    private static final EnumMap<Statistic, StatValues> BASIC_STAT = new EnumMap<Statistic, StatValues>(Statistic.class);
+
+    static {
+        BASIC_STAT.put(Statistic.PV, new StatValuesImpl(BASIC_PV));
+        BASIC_STAT.put(Statistic.COS, new StatValuesImpl(BASIC_COS));
+        BASIC_STAT.put(Statistic.FOR, new StatValuesImpl(BASIC_FOR));
+        BASIC_STAT.put(Statistic.RIFL, new StatValuesImpl(BASIC_RIFL));
+    }
 
     /**
-     * Headless NonPlayerCharacter's constructor.
-     * 
-     * @param basicStat , a map with basic statistics.
-     * @param name      , the name of the non-player character.
+     * Goblin's constructor.
      */
-    public GoblinNPC(final EnumMap<Statistic, StatValuesImpl> basicStat, final String name) {
-        super(basicStat, name);
+    private GoblinNPC() {
+        super("Goblin");
+    }
+
+    /**
+     * Goblin's static factory.
+     * 
+     * @param multiplier it's the rate multiplied at the basic statistics.
+     * @return a new instance of Goblin.
+     */
+    public static BasicCharacter createDarkDestructorNPC(final int multiplier) {
+        final BasicCharacter ret = new GoblinNPC();
+        ret.setBasicStat(BASIC_STAT);
+        // ret.addWeightedAction(new ActionImpl() , RandomActionPrority.DEFAULT);
+        return ret;
     }
 }
