2c830787c27777db973da4c63b28f15ac3101178
Alex Speranza <alex.speranza@studio.unibo.it>
2019-02-28 19:40:17 +0100

    Add method copy to retrieve a new instance of a contraption, copy of the one the method is called on

--- src/model/room_event/Contraption.java
+++ src/model/room_event/Contraption.java
@@ -12,4 +12,10 @@ public interface Contraption extends RoomEvent, ActionActor {
      * Complete the RoomEvent and make it no longer available.
      */
     void complete();
+    
+    /**
+     * @return
+     *  a new instance, copy of the contraption it is called on
+     */
+    Contraption copy();
 }
