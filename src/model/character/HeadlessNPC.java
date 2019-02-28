e58971c77f394f7de44a8ecf4bd8ed3a3ace8aa3
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2019-02-28 23:44:11 +0100

    Add HeadlessNPC's static factory

--- src/model/character/HeadlessNPC.java
+++ src/model/character/HeadlessNPC.java
@@ -2,20 +2,43 @@ package model.character;
 
 import java.util.EnumMap;
 
-
 /**
  * Class that implements a Headless Non-Player Character.
  */
-public class HeadlessNPC extends AbstractCharacter {
+public final  class HeadlessNPC extends AbstractCharacter {
 
-    /**
-     * Headless NonPlayerCharacter's constructor.
-     * 
-     * @param basicStat , a map with basic statistics.
-     * @param name      , the name of the non-player character.
-     */
-    public HeadlessNPC(final EnumMap<Statistic, StatValuesImpl> basicStat, final String name) {
-        super(basicStat, name);
+ // Questi valori sono messi a caso e credo non abbiano assolutamente senso.
+    private static final int BASIC_PV = 50;
+    private static final int BASIC_COS = 30;
+    private static final int BASIC_FOR = 30;
+    private static final int BASIC_RIFL = 20;
+
+    private static final EnumMap<Statistic, StatValues> BASIC_STAT = new EnumMap<Statistic, StatValues>(Statistic.class);
+
+    static {
+        BASIC_STAT.put(Statistic.PV, new StatValuesImpl(BASIC_PV));
+        BASIC_STAT.put(Statistic.COS, new StatValuesImpl(BASIC_COS));
+        BASIC_STAT.put(Statistic.FOR, new StatValuesImpl(BASIC_FOR));
+        BASIC_STAT.put(Statistic.RIFL, new StatValuesImpl(BASIC_RIFL));
     }
 
+    /**
+     * Headless' constructor.
+     */
+    private HeadlessNPC() {
+        super("Headless");
+    }
+
+    /**
+     * Headless' static factory.
+     * 
+     * @param multiplier it's the rate multiplied at the basic statistics.
+     * @return a new instance of Headless.
+     */
+    public static BasicCharacter createDarkDestructorNPC(final int multiplier) {
+        final BasicCharacter ret = new HeadlessNPC();
+        ret.setBasicStat(BASIC_STAT);
+        // ret.addWeightedAction(new ActionImpl() , RandomActionPrority.DEFAULT);
+        return ret;
+    }
 }
