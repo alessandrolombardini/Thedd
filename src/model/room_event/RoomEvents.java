65d78da78488c51edb08762c85788385347f7c18
Alex Speranza <alex.speranza@studio.unibo.it>
2019-03-01 09:00:16 +0100

    Change type of elements of the internal list from Contraption to Supplier<Contraption>. Change static initializer accordingly. Method getTreasureChest now use the ew method newInstance. Method getContraption now uses the supplier given in the initializer

--- src/model/room_event/RoomEvents.java
+++ src/model/room_event/RoomEvents.java
@@ -3,6 +3,7 @@ package model.room_event;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Supplier;
 
 /**
  * Class to create RoomEvents on demand.
@@ -10,11 +11,11 @@ import java.util.Random;
  */
 public class RoomEvents {
 
-    private static final List<Contraption> CONTRAPTION_DATABASE = new ArrayList<>();
+    private static final List<Supplier<Contraption>> CONTRAPTION_DATABASE = new ArrayList<>();
     private static final Random RNG = new Random();
     
     static {
-        CONTRAPTION_DATABASE.add(new ContraptionTrap());
+        CONTRAPTION_DATABASE.add(ContraptionTrap::newInstance);
     }
     
     private RoomEvents() {
@@ -42,7 +43,7 @@ public class RoomEvents {
      *  an instance of {@link model.room_event.TreasureChest}.
      */
     public static final RoomEvent getTreasureChest() {
-        return new TreasureChest();
+        return TreasureChest.newInstance();
     }
     /**
      * 
@@ -50,6 +51,6 @@ public class RoomEvents {
      *  an instance of {@link model.room_event.Contraption}.
      */
     public static final RoomEvent getContraption() {
-            return CONTRAPTION_DATABASE.get(RNG.nextInt(CONTRAPTION_DATABASE.size())).copy();
+            return CONTRAPTION_DATABASE.get(RNG.nextInt(CONTRAPTION_DATABASE.size())).get();
     }
 }
