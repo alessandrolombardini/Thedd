74b80a7bb0288c42ffdfe40d2b1f02a426c21068
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2019-03-04 22:05:49 +0100

    Add EnemyCharacterType enum for enemies

---
+++ src/model/character/EnemyCharacterType.java
@@ -0,0 +1,22 @@
+package model.character;
+
+/**
+ * Enumeration of game's character types.
+ */
+public enum EnemyCharacterType {
+
+    /**
+     * Goblin Non-Player Character.
+     */
+    GOBLIN,
+
+    /**
+     * Dark Destructor Non-Player Character.
+     */
+    DARK_DESTRUCTOR,
+
+    /**
+     * Headless Non-Player Character.
+     */
+    HEADLESS
+}
