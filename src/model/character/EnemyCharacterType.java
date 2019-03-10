84d7c4451b5b33b2bea6973079b6e93763919439
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2019-03-10 17:24:10 +0100

    Add getRandom() method for the enumeration EnemyCharacterType

--- src/model/character/EnemyCharacterType.java
+++ src/model/character/EnemyCharacterType.java
@@ -1,5 +1,7 @@
 package model.character;
 
+import java.util.Random;
+
 /**
  * Enumeration of game's character types.
  */
@@ -18,5 +20,16 @@ public enum EnemyCharacterType {
     /**
      * Headless Non-Player Character.
      */
-    HEADLESS
+    HEADLESS;
+
+    private static final Random RSEED = new Random();
+
+    /**
+     * Get a Random enemy type from the enum.
+     * 
+     * @return a random EnemyCharacterType.
+     */
+    public static EnemyCharacterType getRandom() {
+        return values()[RSEED.nextInt(values().length)];
+    }
 }
