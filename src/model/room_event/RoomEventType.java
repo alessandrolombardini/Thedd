cbe1630350fe5b3089f583066130215b60f0c50a
Alex Speranza <alex.speranza@studio.unibo.it>
2019-03-11 12:09:30 +0100

    Change value of the enum to reflect changes in the respective classes

--- src/model/room_event/RoomEventType.java
+++ src/model/room_event/RoomEventType.java
@@ -8,13 +8,13 @@ public enum RoomEventType {
     /**
      * A combat event, a combat should start when there is an event with this type in a room.
      */
-    COMBAT, 
+    COMBAT_EVENT, 
     /**
      * In the room there are stairs. If stairs are present in a room, then they should be the only event inside that room.
      */
-    STAIRS, 
+    FLOOR_CHANGER_EVENT, 
     /**
      * An event that perform an {@link model.combat.Action}.
      */
-    CONTRAPTION
+    INTERACTABLE_ACTION_PERFORMER
 }
