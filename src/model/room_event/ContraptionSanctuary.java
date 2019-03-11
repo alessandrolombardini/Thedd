b0db548659b18304846430916c00cb34dc2ca590
Alex Speranza <alex.speranza@studio.unibo.it>
2019-03-11 12:13:32 +0100

    Change implementation to extend InteractableActionPerformer. They are not mandatory

--- src/model/room_event/ContraptionSanctuary.java
+++ src/model/room_event/ContraptionSanctuary.java
@@ -7,18 +7,34 @@ import model.combat.implementations.AbstractAction;
 import model.combat.interfaces.Action;
 import model.item.HealingEffect;
 
-public class ContraptionSanctuary extends ContraptionImpl {
+/**
+ * Specialization of {@link model.room_event.InteractableActionPerformer}.
+ * On interaction it fully heals the one or more character.
+ */
+public class ContraptionSanctuary extends AbstractInteractableActionPerformer implements Contraption {
 
     private static final String NAME = "Sanctuary";
     private static final Action ACTION = new AbstractAction(null, "Sanctuary", new ArrayList<>(), 1.0, TargetType.EVERYONE) { };
-    
+
+    /**
+     * Create a new Sanctuary and set his Action to heal everybody in the target's party.
+     */
     public ContraptionSanctuary() {
         super(NAME, ACTION);
         ACTION.addEffect(new HealingEffect(Double.POSITIVE_INFINITY));
     }
-    
+
+    /**
+     * 
+     * @return
+     *  a new instance of this Contraption
+     */
     public static Contraption newInstance() {
         return new ContraptionSanctuary();
     }
-    
+
+    @Override
+    public final boolean isSkippable() {
+        return true;
+    }
 }
--- src/model/room_event/ContraptionTrap.java
+++ src/model/room_event/ContraptionTrap.java
@@ -7,19 +7,35 @@ import model.combat.implementations.AbstractAction;
 import model.combat.implementations.DamageEffect;
 import model.combat.interfaces.Action;
 
-public class ContraptionTrap extends ContraptionImpl {
+/**
+ * Specialization if {@link model.room_event.InteractableActionPerformer}.
+ * It deals damage to the target. 
+ */
+public class ContraptionTrap extends AbstractInteractableActionPerformer implements Contraption {
 
     private static final String NAME = "Trap";
     private static final Action ACTION = new AbstractAction(null, NAME, new ArrayList<>(), 1.0, TargetType.EVERYONE) { }; 
     private static final double BASE_DAMAGE = 20.0;
-            
+
+    /**
+     * Create a new Trap and set the action.
+     */
     public ContraptionTrap() {
         super(NAME, ACTION);
-        this.getAction().get().addEffect(new DamageEffect(BASE_DAMAGE));    
+        this.getAction().get().addEffect(new DamageEffect(BASE_DAMAGE));
     }
-    
+
+    /**
+     * 
+     * @return
+     *  a new instance of this contraption
+     */
     public static Contraption newInstance() {
         return new ContraptionTrap();
     }
-    
+
+    @Override
+    public final boolean isSkippable() {
+        return true;
+    }
 }
