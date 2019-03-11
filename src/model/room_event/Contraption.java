ebdb10b84e7c094b3df0c4a8da391f2b918a4f1d
Alex Speranza <alex.speranza@studio.unibo.it>
2019-03-11 12:11:14 +0100

    Add marker interface for Contraption type. They are a sub-type of InteractableActionPerformer

---
+++ src/model/room_event/Contraption.java
@@ -0,0 +1,10 @@
+package model.room_event;
+
+/**
+ * Logical specialization of {@link model.room_event.InteractableActionPerformer}.
+ * They perform actions that only modify character statistic and status.
+ *
+ */
+public interface Contraption extends InteractableActionPerformer {
+
+}
