78fdcab556d2f6178e243e0c51d173cf305401b7
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-03-13 15:05:18 +0100

    Add documentation, modify hashCode and equals methods, modify methods to conform to interface changes

--- src/model/combat/implementations/ActionResultImpl.java
+++ src/model/combat/implementations/ActionResultImpl.java
@@ -1,37 +1,53 @@
 package model.combat.implementations;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import javafx.util.Pair;
 import model.combat.enums.ActionResultType;
 import model.combat.interfaces.Action;
 import model.combat.interfaces.ActionActor;
 import model.combat.interfaces.ActionResult;
 
+/**
+ * Basic implementation of the ActionResult interface.
+ */
 public class ActionResultImpl implements ActionResult {
 
     private final Action action;
-    private final List<Pair<ActionActor, ActionResultType>> results;
+    private final List<AbstractMap.SimpleImmutableEntry<ActionActor, ActionResultType>> results;
 
+    /**
+     * Public constructor.
+     * @param action the action associated with the result
+     */
     public ActionResultImpl(final Action action) {
         this.action = action;
-        this.results = new ArrayList<Pair<ActionActor, ActionResultType>>();
+        this.results = new ArrayList<AbstractMap.SimpleImmutableEntry<ActionActor, ActionResultType>>();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Action getAction() {
         return action;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void addResult(final ActionActor target, final ActionResultType result) {
-        results.add(new Pair<ActionActor, ActionResultType>(target, result));
+        results.add(new AbstractMap.SimpleImmutableEntry<ActionActor, ActionResultType>(target, result));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public List<Pair<ActionActor, ActionResultType>> getResults() {
+    public List<AbstractMap.SimpleImmutableEntry<ActionActor, ActionResultType>> getResults() {
         return Collections.unmodifiableList(results);
     }
 
