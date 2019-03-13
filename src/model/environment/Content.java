5d21b756b065e34cc58e7120938129b03a03fc9a
Alessandro <alesssandrolombardini@gmail.com>
2019-03-13 20:49:11 +0100

    Add Content enum

---
+++ src/model/environment/Content.java
@@ -0,0 +1,23 @@
+package model.environment;
+
+/**
+ * Representation of objects that could be inside a normal room.
+ * 
+ */
+public enum Content {
+
+    /**
+    * Enemy that the main player have to fight.
+    */
+    ENEMY,
+
+    /**
+     * Treasure that the main player can collect.
+     */
+    TREASURE,
+
+    /**
+     * Contraption that the main player can meet.
+     */
+    CONTRAPTION
+}
