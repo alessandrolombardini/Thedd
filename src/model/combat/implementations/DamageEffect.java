90b707edab5cf69f1f23b1564ab82c3a42a55904
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-03-13 15:15:12 +0100

    Define new methods (to be converted to interface)

--- src/model/combat/implementations/DamageEffect.java
+++ src/model/combat/implementations/DamageEffect.java
@@ -1,41 +1,82 @@
 package model.combat.implementations;
 
+
 import model.combat.interfaces.ActionActor;
-import model.combat.interfaces.ActionEffect;
 
-public class DamageEffect implements ActionEffect {  //to be subdivided into physical damage/magical damage/fire/holy etc...
 
-    private double damage; 
-    private String targetName;
-    private String sourceName;
+/**
+ *  TO BE GENERALIZED AS AN INTERFACE
+ *
+ */
+public class DamageEffect extends AbstractActionEffect {  //to be subdivided into physical damage/magical damage/fire/holy etc...
+
+    private final double baseDamage;
+    private double damage;
 
     public DamageEffect(final double baseAmount) {
-        this.damage = baseAmount;
+        super();
+        baseDamage = baseAmount;
     }
 
     @Override
     public void apply(final ActionActor target) {
         //if target is Character -> target.modifyHealth(amount)
+        damage += baseDamage;
     }
+
+    //GetDamagePreview ?
+    //GetEffectPreview -> "Deals (average) #damage" / "Inflicts status.getName() status" / .. (one for each effect)
 
     @Override
     public void updateEffectByTarget(final ActionActor target) {
+        super.updateEffectByTarget(target);
         // TODO Auto-generated method stub
-        //subtract character armor from damageù
-        //foreach attribute and temporal attribute of the character, attr.modifyValue(damage)
-        targetName = target.getName();
+        //foreach modifier attribute of the character, attr.modifyValue(damage)
     }
 
     @Override
     public void updateEffectBySource(final ActionActor source) {
+        super.updateEffectBySource(source);
         // TODO Auto-generated method stub
-        //add character strenght etc.. to damage
-        sourceName = source.getName();
+        //Override default implementation (but use it!)
+        //add character strenght etc.. to damage (if tag physical, else use faith/willpower)
     }
 
     @Override
     public String getLogMessage() {
-        return sourceName + " succesfully hits " + targetName + " dealing " + damage + " HP damage ";
+        return "Dealt " + damage + " HP damage ";
     }
 
+    public void setDamage(final double newDamage) {
+        damage = newDamage;
+    }
+
+    public void addToDamage(final double damage) {
+        this.damage += damage;
+    }
+
+    //Maybe turn DamageEffect into interface and implement it here
+    public double getBaseDamage() {
+        return baseDamage;
+    }
+
+    public double getDamage() {
+        return damage;
+    }
+
+    /*
+    @Override
+    public boolean equals(final Object other) {
+        if (super.equals(other)) {
+            final DamageEffect o = ((DamageEffect) other);
+            return getBaseDamage() == o.getBaseDamage()
+                    && getDamage() == o.getDamage();
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode() + Objects.hash(getBaseDamage(), getDamage());
+    }*/
 }
