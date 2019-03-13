b448db43ba7913593e10792639aeac2bf297a36a
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-03-13 09:56:16 +0100

    ActionEffect interface: improve documentation, add methods to manage new Tag objects

--- src/model/combat/interfaces/ActionEffect.java
+++ src/model/combat/interfaces/ActionEffect.java
@@ -1,13 +1,17 @@
 package model.combat.interfaces;
 
+import java.util.Set;
+
 /**
+ * 
  * The effect that and Action has on its target.
  * <p>
  * The effect can be applied to the target and may change depending on who is 
- * executing the Action that has this effect and who is the target
+ * executing the Action that has this effect and who is the target.<br>
+ * It is also modified by its various {@link EffectModifiers}.
  *
  */
-public interface ActionEffect {
+public interface ActionEffect extends Modifiable {
 
     /**
      * Applies the effect to the target.
@@ -16,15 +20,13 @@ public interface ActionEffect {
     void apply(ActionActor target);
 
     /**
-     * Updates the magnitude of the effect depending on who
-     * the target is.
+     * Applies to the effect all the target's modifiers that accept this effect.
      * @param target the target of the effects
      */
     void updateEffectByTarget(ActionActor target);
 
     /**
-     * Updates the magnitude of the effect depending on who
-     * the source is.
+     * Applies to the effect all the source's modifiers that accept this effect.
      * @param source the actor executing the action that has this effect
      */
     void updateEffectBySource(ActionActor source);
@@ -33,9 +35,47 @@ public interface ActionEffect {
      * Returns a string describing what the effect did to the targets.
      * <p>
      * Example:<br>
-     * return sourceName + " hits " + targetName + " dealing " + damage + " damage ";
+     * return "Dealt " + damage + " HP damage ";
      * @return a description of what the effect did
      */
     String getLogMessage();
 
+    /**
+     * Gets the Set of target's {@link Tag}.
+     * @return the target's tags
+     */
+    Set<Tag> getTargetTags();
+
+    /**
+     * Gets the Set of source's {@link Tag}.
+     * @return the source's tags
+     */
+    Set<Tag> getSourceTags();
+
+    /**
+     * Gets the Set of {@link Tag} of this effect.
+     * @return the effect's tags
+     */
+    Set<Tag> getTags();
+
+    /**
+     * Adds the specified {@link Tag} to the effect.
+     * @param tag the tag to be added
+     * @param isPermanent true to make the argument tag not removable, false otherwise
+     */
+    void addTag(Tag tag, boolean isPermanent);
+
+    /**
+     * Adds the elements of the specified Set to the effect's {@link Tag} Set.
+     * @param tags the tags to be added
+     * @param arePermanent true to make the argument tags not removable, false otherwise
+     */
+    void addTags(Set<Tag> tags, boolean arePermanent);
+
+    /**
+     * Removes the specified {@link Tag} if it's not permanent.
+     * @param tag the tag to be removed
+     * @return true if the tag is removed, false otherwise
+     */
+    boolean removeTag(Tag tag);
 }
