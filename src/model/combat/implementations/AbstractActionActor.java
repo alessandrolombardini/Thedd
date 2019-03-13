5393661177d9bd111e125ba8d36abe038df7b46a
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-03-13 14:52:41 +0100

    AbstractActionActor: add documentation, remove references to RandomCollections, implemented changes to the interface

--- src/model/combat/implementations/AbstractActionActor.java
+++ src/model/combat/implementations/AbstractActionActor.java
@@ -1,43 +1,70 @@
 package model.combat.implementations;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import model.combat.enums.RandomActionPrority;
 import model.combat.interfaces.Action;
 import model.combat.interfaces.ActionActor;
-import utils.RandomCollection;
-import utils.RandomSet;
-import utils.RandomSetImpl;
+import model.combat.interfaces.Modifier;
+import model.combat.interfaces.Tag;
 
+/**
+ * Abstract implementation of most of common ActionActor functionalities.
+ */
 public abstract class AbstractActionActor implements ActionActor {
 
     private final String name;
+    private final List<Modifier> permanentModifiers = new ArrayList<>();
+    private final List<Modifier> modifiers = new ArrayList<>();
+    private final Set<Tag> permanentTags = new LinkedHashSet<>();
+    private final Set<Tag> tags = new LinkedHashSet<>();
     private Optional<Action> currentAction;
-    private final RandomSet<Action> availableActions = new RandomSetImpl<>();
+    private final Set<Action> availableActions = new LinkedHashSet<Action>();
     private int roundPlace;
     private boolean inCombat;
 
+    /**
+     * Constructor of the class.
+     * @param name the name of the actor.
+     */
     public AbstractActionActor(final String name) {
         this.name = name;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Optional<Action> getAction() {
         return currentAction;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setAction(final Action action) {
         currentAction = Optional.ofNullable(action);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int compareTo(final ActionActor other) {
         return this.getPriority() - other.getPriority();
@@ -46,14 +73,28 @@ public abstract class AbstractActionActor implements ActionActor {
     @Override
     public abstract int getPriority();
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setAvailableActions(final Set<? extends Action> actions) {
         actions.forEach(a -> addAction(a));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public Set<? extends Action> getAvailableActions() {
-        return availableActions.getSet();
+    public List<Action> getAvailableActionsList() {
+        return Collections.unmodifiableSet(availableActions).stream().collect(Collectors.toList());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Set<Action> getAvailableActionsSet() {
+        return Collections.unmodifiableSet(availableActions);
     }
 
     @Override
@@ -65,30 +106,45 @@ public abstract class AbstractActionActor implements ActionActor {
             return false;
         }
         final AbstractActionActor o = ((AbstractActionActor) other);
-        return getName().equals(o.getName());
-                //&& getAvailableActionsList().equals(o.getAvailableActionsList());
+        return getName().equals(o.getName())
+                && getTags().equals(o.getTags())
+                && getAvailableActionsSet().equals(o.getAvailableActionsSet());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getName()/*,getAvailableActionsList()*/);
+        //Name is the only field which doesn't change
+        return Objects.hash(getName());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setIsInCombat(final boolean isInCombat) {
         this.inCombat = isInCombat;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isInCombat() {
         return inCombat;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getPlaceInRound() {
         return roundPlace;
     }
 
+    /**
+     * {@inheritDoc}
+     * {@throws IllegalArgumentException} if the provided number is <= 0
+     */
     @Override
     public void setPlaceInRound(final int place) {
         if (place > 0) {
@@ -98,26 +154,118 @@ public abstract class AbstractActionActor implements ActionActor {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void resetPlaceInRound() {
         roundPlace = 0;
     }
 
-    //PER MARTINA: se intendi rendere sia i personaggi giocanti che non giocanti capaci di eseguire azioni
-    //             random fondendo le clasi AbstractCombatant e AbstractNPCCombatant, questo metodo non ti
-    //             serve ad un cubo probabilmente e puoi semplicemente operare con actionList
-    protected RandomCollection<Action> getWeightedActions() {
-        return availableActions;
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void addAction(final Action action) {
-        availableActions.add(action, RandomActionPrority.DEFAULT.getWeight());
+        availableActions.add(action);
+        action.setSource(this);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean removeAction(final Action action) {
         return availableActions.remove(action);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(Stream.concat(tags.stream(), permanentTags.stream())
+                .collect(Collectors.toSet()));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addTags(final Set<Tag> tags, final boolean arePermanent) {
+        if (arePermanent) {
+            permanentTags.addAll(tags);
+        } else {
+            tags.addAll(tags.stream().filter(permanentTags::contains).collect(Collectors.toSet()));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addTag(final Tag tag, final boolean isPermanent) {
+        if (isPermanent) {
+            permanentTags.add(tag);
+        } else if (!permanentTags.contains(tag)) {
+            tags.add(tag);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean removeTag(final Tag tag) {
+        return tags.remove(tag);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addModifier(final Modifier modifier, final boolean isPermanent) {
+        if (isPermanent) {
+            permanentModifiers.add(modifier);
+        } else if (!permanentModifiers.contains(modifier)){
+            modifiers.add(modifier);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Set<Modifier> getActionModifiers() {
+        return Collections.unmodifiableSet(Stream.concat(permanentModifiers.stream()
+                .filter(m -> m instanceof AbstractActionModifier)
+                .map(m -> (AbstractActionModifier) m),
+                modifiers.stream()
+                .filter(m -> m instanceof AbstractActionModifier)
+                .map(m -> (AbstractActionModifier) m))
+                .collect(Collectors.toSet()));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Set<Modifier> getEffectModifiers() {
+        return Collections.unmodifiableSet(Stream.concat(permanentModifiers.stream()
+                .filter(m -> m instanceof AbstractEffectModifier)
+                .map(m -> (AbstractEffectModifier) m),
+                modifiers.stream()
+                .filter(m -> m instanceof AbstractEffectModifier)
+                .map(m -> (AbstractEffectModifier) m))
+                .collect(Collectors.toSet()));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void removeModifier(final Modifier modifier) {
+        modifiers.remove(modifier);
     }
 
 }
