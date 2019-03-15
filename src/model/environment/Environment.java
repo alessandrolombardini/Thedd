5061a6d9a01edd1411ad0921686f748d647f2863
Alessandro <alesssandrolombardini@gmail.com>
2019-03-15 21:30:44 +0100

    Refactor of Environment class and interface: now it accept FloorDetails instead of Floor

--- src/model/environment/Environment.java
+++ src/model/environment/Environment.java
@@ -1,5 +1,7 @@
 package model.environment;
 
+import java.util.List;
+
 /**
  * Interface that define the environment.
  *
@@ -9,22 +11,34 @@ public interface Environment {
     /***
      * This method allows to set the next floor.
      * 
-     * @param floor Next floor
-     * @throws NullPointerExeption if floor is null
+     * @param floorDetails is the details of the floor that has to be setted
+     * @throws NullPointerExeption   if floorDetails is null
+     * @throws IllegalStateException if the current floor isn't completed
      */
-    void setNextFloor(Floor floor);
+    void setNextFloor(FloorDetails floorDetails);
 
     /**
      * This method allows to get the current floor.
      * 
      * @return the current floor
+     * @throws IllegalStateException if there aren't floors
      */
     Floor getCurrentFloor();
 
     /**
      * This method allows to get the current floor index.
+     * The index is -1 if there aren't floor yet.
      * 
      * @return the current floor index
      */
     int getCurrentFloorIndex();
+
+    /**
+     * This method allows to know if the current floor is the last.
+     * 
+     * @return true if the current floor is the last
+     */
+    boolean isCurrentLastFloor();
+
+
 }
--- src/model/environment/EnvironmentImpl.java
+++ src/model/environment/EnvironmentImpl.java
@@ -3,6 +3,8 @@ package model.environment;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Implementation of {@link model.environment.Environment}.
@@ -10,31 +12,46 @@ import java.util.Objects;
  */
 public class EnvironmentImpl implements Environment {
 
+    /**
+     * This constant define the minimum number of rooms for each level. 
+     */
+    public static final int MIN_NUMBER_OF_ROOMS = 1;
+    /**
+     * This constant define the minimim number of level for the map.
+     */
+    public static final int MIN_NUMBER_OF_FLOORS = 1;
+
+    private static final int NONE_FLOOR = -1;
+
     private final List<Floor> floors;
+    private final FloorDetailsFactory floorDeatailsFactory;
+    private final int numberOfFloors;
+    private final int numberOfRooms;
     private int actuaIndexFloor;
 
     /**
-     * Environment constructor.
+     * EnvironmentImpl constructor.
      * 
-     * @param floor is the first floor of the environment
-     * @throws NullPointerExeption if floor is null
+     * @param numberOfFloors is the number of floors of the map
+     * @param numberOfRooms  is the number of rooms of each floor
+     * @throws IllegalArgumentException if the number of floors or rooms is not valid
      */
-    public EnvironmentImpl(final Floor floor) {
-        Objects.requireNonNull(floor);
+    public EnvironmentImpl(final int numberOfFloors, final int numberOfRooms) {
+        if (numberOfFloors < MIN_NUMBER_OF_FLOORS || numberOfRooms < MIN_NUMBER_OF_ROOMS) {
+            throw new IllegalArgumentException();
+        }
         this.floors = new ArrayList<>();
-        this.actuaIndexFloor = 0;
-        this.floors.add(this.actuaIndexFloor, floor);
-    }
-
-    @Override
-    public final void setNextFloor(final Floor floor) {
-        Objects.requireNonNull(floor);
-        this.actuaIndexFloor++;
-        this.floors.add(actuaIndexFloor, floor);
+        this.floorDeatailsFactory = new FloorDetailsFactoryImpl();
+        this.numberOfFloors = numberOfFloors;
+        this.numberOfRooms = numberOfRooms;
+        this.actuaIndexFloor = NONE_FLOOR;
     }
 
     @Override
     public final Floor getCurrentFloor() {
+        if (this.getCurrentFloorIndex() == NONE_FLOOR) {
+            throw new IllegalStateException();
+        }
         return this.floors.get(this.actuaIndexFloor);
     }
 
@@ -42,5 +59,26 @@ public class EnvironmentImpl implements Environment {
     public final int getCurrentFloorIndex() {
         return this.actuaIndexFloor;
     }
+
+    @Override
+    public final void setNextFloor(final FloorDetails floorDetails) {
+        Objects.requireNonNull(floorDetails);
+        if (!this.floors.get(this.actuaIndexFloor).checkToChangeFloor()) {
+            throw new IllegalStateException();
+        }
+        this.actuaIndexFloor++;
+        this.floors.add(this.actuaIndexFloor, new FloorImpl(floorDetails, this.numberOfRooms));
+    }
+
+    @Override
+    public final boolean isCurrentLastFloor() {
+        return this.getCurrentFloorIndex() == (this.numberOfFloors - 1);
+    }
+
+    private boolean isNextLastFloor() {
+        return (this.getCurrentFloorIndex() + 1) == (this.numberOfFloors - 1);
+    }
+
+
 
 }
