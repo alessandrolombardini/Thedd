961ffa84492742b61705142aa695bacf0cac690f
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-03-16 08:47:40 +0100

    AbstractAutomaticActor: convert private class WeightedItem to a decorator for Actions

--- src/model/combat/actor/AbstractAutomaticActor.java
+++ src/model/combat/actor/AbstractAutomaticActor.java
@@ -1,14 +1,15 @@
 package model.combat.actor;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
 
 import model.combat.action.Action;
-import model.combat.action.ActionImpl;
 import model.combat.action.TargetType;
 import model.combat.action.effect.ActionEffect;
 import model.combat.common.CombatInstance;
+import model.combat.tag.Tag;
 import utils.RandomPrority;
 import utils.randomcollection.RandomCollection;
 import utils.randomcollection.RandomSet;
@@ -107,22 +108,20 @@ public abstract class AbstractAutomaticActor extends AbstractActionActor impleme
         getAction().get().setTargets(availableTargets.get(targetIndex), availableTargets);
     }
 
-    private class WeightedAction extends ActionImpl implements WeightedItem<Action> {
+    private RandomSet<Action> getRandomSet() {
+        final RandomSet<Action> set = new RandomSetImpl<>();
+        set.addAll(getAvailableActionsSet());
+        return set;
+    }
+
+    private class WeightedAction implements Action, WeightedItem<Action> {
 
         private double weight;
+        private Action action;
 
         WeightedAction(final Action action, final double weight) {
-            this(action.getSource().orElse(null),
-                action.getName(), action.getEffects(), action.getBaseHitChance(),
-                action.getTargetType(), action.getDescription(), action.getLogMessage(), weight);
             this.weight = weight;
-        }
-
-        WeightedAction(final ActionActor source, final String name, final List<ActionEffect> effects,
-                final double baseHitChance, final TargetType targetType,
-                final String description, final String logMessage, final double weight) {
-            super(source, name, effects, baseHitChance, targetType, description, logMessage);
-            this.weight = weight;
+            this.action = action;
         }
 
         @Override
@@ -137,13 +136,138 @@ public abstract class AbstractAutomaticActor extends AbstractActionActor impleme
 
         @Override
         public Action getItem() {
-            return this;
+            return action;
+        }
+
+        @Override
+        public void setTargets(final ActionActor target, final List<ActionActor> targetedParty) {
+            action.setTargets(target, targetedParty);
+        }
+
+        @Override
+        public void setSource(final ActionActor source) {
+            action.setSource(source);
+        }
+
+        @Override
+        public Optional<ActionActor> getSource() {
+            return action.getSource();
+        }
+
+        @Override
+        public List<ActionEffect> getEffects() {
+            return action.getEffects();
+        }
+
+        @Override
+        public void addEffects(final List<ActionEffect> effects) {
+            action.addEffects(effects);
+        }
+
+        @Override
+        public void addEffect(final ActionEffect effect) {
+            action.addEffect(effect);
+        }
+
+        @Override
+        public void removeEffect(final ActionEffect effect) {
+            action.removeEffect(effect);
+        }
+
+        @Override
+        public void applyEffects() {
+            action.applyEffects();
+        }
+
+        @Override
+        public String getName() {
+            return action.getName();
+        }
+
+        @Override
+        public List<ActionActor> getTargets() {
+            return action.getTargets();
+        }
+
+        @Override
+        public double getHitChanceModifier() {
+            return action.getHitChanceModifier();
+        }
+
+        @Override
+        public TargetType getTargetType() {
+            return action.getTargetType();
+        }
+
+        @Override
+        public void setTargetType(final TargetType targetType) {
+            action.setTargetType(targetType);
+        }
+
+        @Override
+        public boolean isTargetHit() {
+            return action.isTargetHit();
+        }
+
+        @Override
+        public void rollToHit() {
+            action.rollToHit();
+        }
+
+        @Override
+        public List<ActionActor> getValidTargets(final CombatInstance combatInstance) {
+            return action.getValidTargets(combatInstance);
+        }
+
+        @Override
+        public void addTag(final Tag tag) {
+            action.addTag(tag);
+        }
+
+        @Override
+        public void addTags(final Set<Tag> tags) {
+            action.addTags(tags);
+        }
+
+        @Override
+        public Set<Tag> getTags() {
+            return action.getTags();
+        }
+
+        @Override
+        public double getBaseHitChance() {
+            return action.getBaseHitChance();
+        }
+
+        @Override
+        public void selectNextTarget() {
+            action.selectNextTarget();
+        }
+
+        @Override
+        public ActionActor getCurrentTarget() {
+            return action.getCurrentTarget();
+        }
+
+        @Override
+        public String getLogMessage() {
+            return action.getLogMessage();
+        }
+
+        @Override
+        public String getDescription() {
+            return action.getDescription();
+        }
+
+        @Override
+        public boolean equals(final Object other) {
+            return action.equals(other);
+        }
+
+        @Override
+        public int hashCode() {
+            return action.hashCode();
         }
     }
 
-    private RandomSet<Action> getRandomSet() {
-        final RandomSet<Action> set = new RandomSetImpl<>();
-        set.addAll(getAvailableActionsSet());
-        return set;
-    }
 }
