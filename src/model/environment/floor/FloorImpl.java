e0e84c2a150e606c29bed67336e9757b33d69b03
Alessandro <alesssandrolombardini@gmail.com>
2019-03-16 12:43:43 +0100

    Add to FloorImpl constructor the fied isLastFloor and update fields check in its method

--- src/model/environment/floor/FloorImpl.java
+++ src/model/environment/floor/FloorImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import model.environment.environment.EnvironmentImpl;
 import model.environment.room.Room;
 import model.environment.room.RoomFactory;
 import model.environment.room.RoomFactoryImpl;
@@ -15,6 +16,8 @@ import model.environment.room.RoomFactoryImpl;
  */
 public class FloorImpl implements Floor {
 
+    private static final int NONE_ROOMS = -1;
+
     private final RoomFactory factory;
     private final List<Room> rooms;
     private final int numberOfRooms;
@@ -23,30 +26,33 @@ public class FloorImpl implements Floor {
     /**
      * Floor constructor.
      * 
-     * @param floorDetails contains the details of this floor
+     * @param floorDetails  contains the details of this floor
      * @param numberOfRooms define the number of rooms of the floor
-     * @throws NullPointerExecption if roomFactory is null
-     * @throws IllegalStateExeption if floor doesn't contains rooms
+     * @param isLastFloor   is true if this is the last floor
+     * @throws NullPointerExecption    if roomFactory is null
+     * @throws IllegalArgumentExeption if floor doesn't contains enought rooms
      */
-    public FloorImpl(final FloorDetails floorDetails, final int numberOfRooms) {
+    public FloorImpl(final FloorDetails floorDetails, final int numberOfRooms, final boolean isLastFloor) {
         Objects.requireNonNull(floorDetails);
-        this.factory = new RoomFactoryImpl(floorDetails, numberOfRooms);
+        if (numberOfRooms < EnvironmentImpl.MIN_NUMBER_OF_ROOMS) {
+            throw new IllegalArgumentException();
+        }
+        this.factory = new RoomFactoryImpl(floorDetails, numberOfRooms, isLastFloor);
         this.rooms = new ArrayList<>();
         this.numberOfRooms = numberOfRooms;
-        this.currentRoomIndex = -1;
-        this.nextRoom();
+        this.currentRoomIndex = NONE_ROOMS;
     }
 
     @Override
     public final boolean hasNextRoom() {
-        return this.currentRoomIndex < this.numberOfRooms;
+        return this.currentRoomIndex < (this.numberOfRooms - 1);
     }
 
     @Override
     public final boolean nextRoom() {
         if (!this.hasNextRoom()) {
             throw new IllegalStateException();
-        } else if (!this.rooms.isEmpty() && !this.rooms.get(this.currentRoomIndex).checkToMoveOn()) {
+        } else if (!this.rooms.get(this.currentRoomIndex).checkToMoveOn()) {
             return false;
         }
         this.setNextRoom();
@@ -55,6 +61,9 @@ public class FloorImpl implements Floor {
 
     @Override
     public final Room getCurrentRoom() {
+        if (this.currentRoomIndex == NONE_ROOMS) {
+            throw new IllegalStateException();
+        }
         return this.rooms.get(this.currentRoomIndex);
     }
 
@@ -63,14 +72,14 @@ public class FloorImpl implements Floor {
         return this.currentRoomIndex;
     }
 
+    @Override
+    public final boolean checkToChangeFloor() {
+        return !this.hasNextRoom() && this.rooms.get(this.currentRoomIndex).checkToMoveOn();
+    }
+
     private void setNextRoom() {
         this.currentRoomIndex++;
         this.rooms.add(this.currentRoomIndex, this.factory.createRoom());
-    }
-
-    @Override
-    public boolean checkToChangeFloor() {
-        return false;
     }
 
 }
