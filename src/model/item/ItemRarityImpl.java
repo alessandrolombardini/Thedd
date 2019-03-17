070c0f5d30a4096d17a49f0587c597d1e200c25e
Alex Speranza <alex.speranza@studio.unibo.it>
2019-03-17 16:17:44 +0100

    Add enumeration which implements ItemRarity

---
+++ src/model/item/ItemRarityImpl.java
@@ -0,0 +1,39 @@
+package model.item;
+
+/**
+ * 
+ *
+ */
+public enum ItemRarityImpl implements ItemRarity {
+    /**
+     * The least rare. It has the highest weight.
+     */
+    COMMON("Common", 6),
+    /**
+     * The median rare. It has an average weight.
+     */
+    UNCOMMON("Uncommon", 3),
+    /**
+     * The rarest. It has the lowest weight.
+     */
+    RARE("Rare", 3);
+
+    private final int baseWeight;
+    private final String literal;
+
+    ItemRarityImpl(final String literal, final int baseWeight) {
+        this.baseWeight = baseWeight;
+        this.literal = literal;
+    }
+
+    @Override
+    public String getLiteral() {
+        return literal;
+    }
+
+    @Override
+    public int getBaseWeight() {
+        return baseWeight;
+    }
+
+}
