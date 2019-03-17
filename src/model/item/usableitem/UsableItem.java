5b1ad919dffc83b6e99ae21a0ec13d8ae95cf08a
Alex Speranza <alex.speranza@studio.unibo.it>
2019-03-17 17:05:32 +0100

    Add method getEffectMultipliers to get the multipliers applied on effects based on the rarity

--- src/model/item/usableitem/UsableItem.java
+++ src/model/item/usableitem/UsableItem.java
@@ -1,7 +1,11 @@
 package model.item.usableitem;
 
+import java.util.Map;
+
 import model.combat.action.Action;
+import model.combat.action.effect.ActionEffect;
 import model.item.Item;
+import model.item.ItemRarity;
 
 /**
  * Specialized interface of {@link model.item.Item} of usable items.
@@ -13,5 +17,19 @@ public interface UsableItem extends Item {
      * @return the action to apply to a target
      */
     Action getAction();
+
+    /**
+     * 
+     * @param effect
+     *  the effect to add to the item action
+     */
+    void addActionEffect(ActionEffect effect);
+
+    /**
+     * 
+     * @return
+     *  the map of the effect multipliers based on the rarity
+     */
+    Map<ItemRarity, Integer> getEffectsMultiplier();
 
 }
