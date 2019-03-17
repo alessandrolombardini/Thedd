e6106d8c3697ea51d498dfed0e0673816148a91f
Alex Speranza <alex.speranza@studio.unibo.it>
2019-03-17 17:12:04 +0100

    Add extension to AbstractActionEffect. Remove method implemented in the abstract class. Add check to the base healing given to the contructor. Now the effect is a percentage

--- src/model/item/HealingEffect.java
+++ src/model/item/HealingEffect.java
@@ -2,50 +2,49 @@ package model.item;
 
 import model.character.BasicCharacter;
 import model.character.Statistic;
-import model.combat.interfaces.ActionActor;
-import model.combat.interfaces.ActionEffect;
+import model.combat.action.effect.AbstractActionEffect;
+import model.combat.actor.ActionActor;
+import model.combat.action.effect.ActionEffect;
 
 /**
  * ActionEffect which heals the target.
  *
  */
-public class HealingEffect implements ActionEffect {
+public final class HealingEffect extends AbstractActionEffect implements ActionEffect {
 
     private final double baseHealing;
 
+    /**
+     * 
+     * @param healingValue
+     *          percentage value of health healed to to the target.
+     */
     public HealingEffect(final double healingValue) {
+        if (healingValue < 0.0 || healingValue > 1.0) {
+            throw new IllegalStateException("Healing value must be between 0.0 and 1.0, as it is a percentage");
+        }
         baseHealing = healingValue;
     }
 
     @Override
-    public final void apply(final ActionActor target) {
+    public void apply(final ActionActor target) {
         if (target instanceof BasicCharacter) {
             final BasicCharacter t = ((BasicCharacter) target);
-            if(baseHealing != Double.POSITIVE_INFINITY) {
-                t.getStat(Statistic.PV).updateActual((int) baseHealing);
-            } else {
-                t.getStat(Statistic.PV).updateActual(t.getStat(Statistic.PV).getMax());
-            }
+            final int roundedFlatHealing = (int) Math.round(t.getStat(Statistic.PV).getMax() * baseHealing);
+            t.getStat(Statistic.PV).updateActual(roundedFlatHealing);
         } else {
             throw new IllegalArgumentException("Target must be a BasicCharacter");
         }
     }
 
     @Override
-    public void updateEffectByTarget(final ActionActor target) {
-        // TODO Auto-generated method stub
-
+    public String getLogMessage() {
+        return " is healed for " + baseHealing + "% max PV.";
     }
 
     @Override
-    public void updateEffectBySource(final ActionActor source) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public final String getLogMessage() {
-        return " is healed for " + baseHealing + " PV.";
+    public String getDescription() {
+        return null;
     }
 
 }
