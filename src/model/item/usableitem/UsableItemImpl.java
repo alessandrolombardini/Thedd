33254acd5f44d025a759a447f4cd3ce68feedf76
Alex Speranza <alex.speranza@studio.unibo.it>
2019-03-17 17:07:02 +0100

    Add map of effect multipliers and implements method to return it. The contructor now accept an ItemRarity to conform to AbstractItem contructor

--- src/model/item/usableitem/UsableItemImpl.java
+++ src/model/item/usableitem/UsableItemImpl.java
@@ -1,14 +1,19 @@
 package model.item.usableitem;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-import model.combat.action.TargetType;
-import model.combat.action.ActionImpl;
 import model.combat.action.Action;
+import model.combat.action.ActionImpl;
+import model.combat.action.TargetType;
 import model.combat.action.effect.ActionEffect;
 import model.item.AbstractItem;
-import model.item.Item;
+import model.item.ItemRarity;
+import model.item.ItemRarityImpl;
 
 /**
  * Implementation of {@link model.item.usableitem.UsableItem}.
@@ -16,34 +21,50 @@ import model.item.Item;
  */
 public class UsableItemImpl extends AbstractItem implements UsableItem {
 
+    private static final Map<ItemRarity, Integer> EFFECT_MULTIPLIERS;
+    private final List<ActionEffect> effects;
+
+    static {
+        EFFECT_MULTIPLIERS = new HashMap<>();
+        EFFECT_MULTIPLIERS.put(ItemRarityImpl.COMMON, 1);
+        EFFECT_MULTIPLIERS.put(ItemRarityImpl.UNCOMMON, 2);
+        EFFECT_MULTIPLIERS.put(ItemRarityImpl.RARE, 4);
+    }
+
     /**
      * 
      * @param id
      *  id of the object
      * @param name
      *  name of the item
-     * @param effects
-     *  map of effects of the item
+     * @param rarity
+     *  rarity of the item
      * @param description
      *  description of the Item
      */
-    public UsableItemImpl(final int id, final String name, final List<ActionEffect> effects, final String description) {
-        super(id, name, effects, description);
+    public UsableItemImpl(final int id, final String name, final ItemRarity rarity, final String description) {
+        super(id, name, rarity, description);
+        effects = new ArrayList<>();
     }
 
     @Override
     public final Action getAction() {
-        return new ActionImpl(null, this.getName(), this.getEffects(), 1, TargetType.EVERYONE, this.getDescription(), " used " + this.getName());
-    }
-
-    @Override
-    public final Item copy() {
-        return new UsableItemImpl(this.getId(), this.getName(), this.getEffects(), this.getDescription());
+        return new ActionImpl(null, this.getName(), effects, 1, TargetType.EVERYONE, this.getDescription(), " used " + this.getName());
     }
 
     @Override
     public final String toString() {
-        return this.getName() + ": " + this.getEffects().stream().map(e -> e.getLogMessage()).collect(Collectors.joining(", ", "[", "]")) + " | " + this.getDescription();
+        return this.getName() + ": " + effects.stream().map(e -> e.getLogMessage()).collect(Collectors.joining(", ", "[", "]")) + " | " + this.getDescription();
+    }
+
+    @Override
+    public final void addActionEffect(final ActionEffect effect) {
+        effects.add(effect);
+    }
+
+    @Override
+    public final Map<ItemRarity, Integer> getEffectsMultiplier() {
+        return Collections.unmodifiableMap(EFFECT_MULTIPLIERS);
     }
 
 }
