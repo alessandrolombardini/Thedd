ab7c5b630c22bf8d103447707543b7cdacfeddb1
Alex Speranza <alex.speranza@studio.unibo.it>
2019-03-18 08:59:26 +0100

    Change weight of RARE rarity: it was higher than intended. Remove static method to get a random rarity: the random extraction is moved to the item factory

--- src/model/item/ItemRarityImpl.java
+++ src/model/item/ItemRarityImpl.java
@@ -1,7 +1,5 @@
 package model.item;
 
-import java.util.Random;
-
 /**
  * 
  *
@@ -18,7 +16,7 @@ public enum ItemRarityImpl implements ItemRarity {
     /**
      * The rarest. It has the lowest weight.
      */
-    RARE("Rare", 3);
+    RARE("Rare", 1);
 
     private final int baseWeight;
     private final String literal;
@@ -36,15 +34,5 @@ public enum ItemRarityImpl implements ItemRarity {
     @Override
     public int getBaseWeight() {
         return baseWeight;
-    }
-
-    /**
-     * 
-     * @return
-     *  a random rarity from the values of this enumeration
-     */
-    public static ItemRarity getRandomRarity() {
-       final Random r = new Random();
-       return ItemRarityImpl.values()[r.nextInt(ItemRarityImpl.values().length)]; 
     }
 }
