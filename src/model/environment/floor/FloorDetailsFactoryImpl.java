8fd2301bc2f351495d7ee96fe2e3cb3cfc4eb587
Alessandro <alesssandrolombardini@gmail.com>
2019-03-19 10:23:43 +0100

    Refactor of class FloorDetailsFactoryImpl: remove random and use RandomUtil and change the way it builds a new FloorDetails

--- src/model/environment/floor/FloorDetailsFactoryImpl.java
+++ src/model/environment/floor/FloorDetailsFactoryImpl.java
@@ -3,9 +3,7 @@ package model.environment.floor;
 import java.util.EnumMap;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Random;
 
-import model.environment.floor.FloorDetails.Builder;
 import model.environment.enums.Difficulty;
 import model.environment.environment.EnvironmentImpl;
 
@@ -26,66 +24,49 @@ public final class FloorDetailsFactoryImpl implements FloorDetailsFactory {
         MULTIPLIER.put(Difficulty.HARD, HARD_MODE_MULTIPLIER);
     }
 
-    private final Random random;
-    private Optional<Integer> effectiveRooms;
+    private Optional<Integer> effectiveNumberOfRooms;
     private Optional<Difficulty> difficulty;
-    private Optional<Builder> builder;
 
     /**
      * FloorDetailsFactory constructor.
      */
     public FloorDetailsFactoryImpl() {
-        random = new Random(System.currentTimeMillis());
-        this.effectiveRooms = Optional.empty();
+        this.effectiveNumberOfRooms = Optional.empty();
         this.difficulty = Optional.empty();
-        this.builder = Optional.empty();
     }
 
     @Override
     public FloorDetails createFloorDetails(final Difficulty difficulty, final int numberOfRooms, final boolean lastFloor) {
         Objects.requireNonNull(difficulty);
         if (numberOfRooms < EnvironmentImpl.MIN_NUMBER_OF_ROOMS) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("The number of rooms is not valid");
         }
-        this.effectiveRooms = Optional.of(numberOfRooms - 1);
+        this.effectiveNumberOfRooms = Optional.of(numberOfRooms - 1);
         this.difficulty = Optional.of(difficulty);
-        this.builder = Optional.of(new FloorDetails.Builder());
-        this.setValues();
-        return builder.get().build();
-    }
-
-    private void setValues() {
-        this.builder.get().difficulty(this.difficulty.get());
-        this.builder.get().enemies(this.getRandomNumberOfEnemies());
-        this.builder.get().contraptions(this.getRandomNumberOfContraptions());
-        this.builder.get().treasures(this.getRandomNumberOfTreasure());
+        return new FloorDetails.Builder().difficulty(this.difficulty.get())
+                                         .enemies(this.getRandomNumberOfEnemies())
+                                         .contraptions(this.getRandomNumberOfContraptions())
+                                         .treasures(this.getRandomNumberOfTreasure())
+                                         .build();
     }
 
     private int getRandomNumberOfEnemies() {
-        return (int) Math.round(this.effectiveRooms.get() * MULTIPLIER.get(this.difficulty.get()))
-                + this.getRandomRoundingNumber();
+        return (int) Math.round(this.effectiveNumberOfRooms.get() * MULTIPLIER.get(this.difficulty.get())) 
+                + RandomUtils.getRandomInteger((int) Math.round(this.effectiveNumberOfRooms.get() * MULTIPLIER.get(Difficulty.EASY)));
     }
 
     private int getRandomNumberOfTreasure() {
-        return (int) Math.round(this.getGaussianRoundingNumber() * MULTIPLIER.get(this.difficulty.get()));
+        return (int) Math.round(RandomUtils.getRandomGaussianNumber(this.getFloorMultiplierValue(), this.getFloorMultiplierValue())
+                * MULTIPLIER.get(this.difficulty.get()));
     }
 
     private int getRandomNumberOfContraptions() {
-        return (int) Math.round(this.getGaussianRoundingNumber() * MULTIPLIER.get(this.difficulty.get()));
+        return (int) Math.round(RandomUtils.getRandomGaussianNumber(this.getFloorMultiplierValue(), this.getFloorMultiplierValue())
+                        * MULTIPLIER.get(this.difficulty.get()));
     }
 
-    private int getRandomRoundingNumber() {
-        return (int) this.random
-                .nextInt((int) Math.round(this.effectiveRooms.get() * MULTIPLIER.get(Difficulty.EASY)) + 1);
-    }
-
-    private int getGaussianRoundingNumber() {
-        return (int) Math.round(
-                random.nextGaussian() * Math.sqrt(this.getRoundingNumberConstant()) + this.getRoundingNumberConstant());
-    }
-
-    private int getRoundingNumberConstant() {
-        return (int) Math.round(this.effectiveRooms.get() * MULTIPLIER.get(Difficulty.NORMAL));
+    private int getFloorMultiplierValue() {
+        return (int) Math.round(this.effectiveNumberOfRooms.get() * MULTIPLIER.get(Difficulty.NORMAL));
     }
 
 }
