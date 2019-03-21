9ffb6d82f9d83a607f13c8e4294483421958c237
Alex Speranza <alex.speranza@studio.unibo.it>
2019-03-21 09:23:59 +0100

    Add class ModifierAdderEffect. It is an effect which adds a modifier to the target. It implements ActionEffect and StatisticBonusEffect and extends AbstractActionEffect

---
+++ src/model/item/ModifierAdderEffect.java
@@ -0,0 +1,61 @@
+package model.item;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import model.combat.action.effect.AbstractActionEffect;
+import model.combat.action.effect.ActionEffect;
+import model.combat.actor.ActionActor;
+import model.combat.modifier.Modifier;
+
+/**
+ * {@link model.combat.action.effect.ActionEffect} which adds one {@link model.combat.modifier.Modifier}
+ * to every actions of an {@link model.combat.actor.ActionActor} with the {@link model.combat.tag.Tag}
+ * specified in the modifier.
+ */
+public class ModifierAdderEffect extends AbstractActionEffect implements ActionEffect, StatisticBonusEffect {
+
+    private final Modifier modifier;
+    private final boolean isPermanent;
+    private Optional<ActionActor> target;
+
+    /**
+     * Create a new {@link model.combat.action.effect.ActionEffect}.
+     * @param modifier
+     *          the modifier to be added to the target
+     * @param isPermanent
+     *          whether the modifier is permanent
+     */
+    public ModifierAdderEffect(final Modifier modifier, final boolean isPermanent) {
+        this.modifier = modifier;
+        this.isPermanent = isPermanent;
+        target = Optional.empty();
+    }
+
+    @Override
+    public final String getDescription() {
+        return modifier.toString();
+    }
+
+    @Override
+    public final void apply(final ActionActor target) {
+        this.target = Optional.of(Objects.requireNonNull(target));
+        this.target.get().addModifier(modifier, isPermanent);
+    }
+
+    @Override
+    public final void removeBonus() {
+        if (!target.isPresent()) {
+            throw new IllegalStateException("This effect is not applied to any target;");
+        } else {
+            if (!isPermanent) {
+                this.target.get().removeModifier(modifier);
+            }
+        }
+    }
+
+    @Override
+    public final String getLogMessage() {
+        return null;
+    }
+}
