4fc5dcfc302e02112ff23e31d6d49b7493accd53
Alessandro <alesssandrolombardini@gmail.com>
2019-03-23 14:15:18 +0100

    Refactor of FloorDetailFactoryImpl: change use of FloorDetails and change operations and methods to get random value of enemies, contraptions and treasures

--- src/model/environment/floor/FloorDetailsFactoryImpl.java
+++ src/model/environment/floor/FloorDetailsFactoryImpl.java
@@ -2,71 +2,80 @@ package model.environment.floor;
 
 import java.util.EnumMap;
 import java.util.Objects;
-import java.util.Optional;
 
 import model.environment.enums.Difficulty;
-import model.environment.environment.EnvironmentImpl;
+import model.environment.room.RoomFactoryImpl;
 
 /**
- * This class represents all details of the floor.
+ * This class is a factory of random details of a new floor based on value that the constructor recive.
  *
  */
 public final class FloorDetailsFactoryImpl implements FloorDetailsFactory {
 
+    private static final EnumMap<Difficulty, Double> MULTIPLIER = new EnumMap<Difficulty, Double>(Difficulty.class);
     private static final double EASY_MODE_MULTIPLIER = 0.25;
     private static final double NORMAL_MODE_MULTIPLIER = 0.5;
     private static final double HARD_MODE_MULTIPLIER = 1;
-    private static final EnumMap<Difficulty, Double> MULTIPLIER;
+
+    private static final String ERROR_NONVALIDROOMS = "Number of rooms is not valid";
+
     static {
-        MULTIPLIER = new EnumMap<Difficulty, Double>(Difficulty.class);
         MULTIPLIER.put(Difficulty.EASY, EASY_MODE_MULTIPLIER);
         MULTIPLIER.put(Difficulty.NORMAL, NORMAL_MODE_MULTIPLIER);
         MULTIPLIER.put(Difficulty.HARD, HARD_MODE_MULTIPLIER);
     }
 
-    private Optional<Integer> effectiveNumberOfRooms;
-    private Optional<Difficulty> difficulty;
-
     /**
      * FloorDetailsFactory constructor.
      */
-    public FloorDetailsFactoryImpl() {
-        this.effectiveNumberOfRooms = Optional.empty();
-        this.difficulty = Optional.empty();
-    }
+    public FloorDetailsFactoryImpl() { }
 
     @Override
     public FloorDetails createFloorDetails(final Difficulty difficulty, final int numberOfRooms, final boolean lastFloor) {
         Objects.requireNonNull(difficulty);
-        if (numberOfRooms < EnvironmentImpl.MIN_NUMBER_OF_ROOMS) {
-            throw new IllegalArgumentException("The number of rooms is not valid");
+        if (numberOfRooms <= 0) {
+            throw new IllegalArgumentException(ERROR_NONVALIDROOMS);
         }
-        this.effectiveNumberOfRooms = Optional.of(numberOfRooms - 1);
-        this.difficulty = Optional.of(difficulty);
-        return new FloorDetails.Builder().difficulty(this.difficulty.get())
-                                         .enemies(this.getRandomNumberOfEnemies())
-                                         .contraptions(this.getRandomNumberOfContraptions())
-                                         .treasures(this.getRandomNumberOfTreasure())
+        final int numberOfContraptions = this.getRandomNumberOfContraptions(numberOfRooms - 1, difficulty, 0);
+        final int numberOfTreasures = this.getRandomNumberOfTreasure(numberOfRooms - 1, difficulty, numberOfContraptions);
+        return new FloorDetails.Builder().difficulty(difficulty)
+                                         .rooms(numberOfRooms)
+                                         .boss(lastFloor)
+                                         .enemies(this.getRandomNumberOfEnemies(numberOfRooms - 1, difficulty))
+                                         .contraptions(numberOfContraptions)
+                                         .treasures(numberOfTreasures)
                                          .build();
     }
 
-    private int getRandomNumberOfEnemies() {
-        return (int) Math.round(this.effectiveNumberOfRooms.get() * MULTIPLIER.get(this.difficulty.get())) 
-                + RandomUtils.getRandomInteger((int) Math.round(this.effectiveNumberOfRooms.get() * MULTIPLIER.get(Difficulty.EASY)));
+    private int getRandomNumberOfEnemies(final int effectiveNumberOfRooms, final Difficulty difficulty) {
+        final int baseNumber = (int) Math.round(effectiveNumberOfRooms * MULTIPLIER.get(difficulty));
+        final int maxRandomRoundIntegerNumber = (int) Math.round(effectiveNumberOfRooms * MULTIPLIER.get(Difficulty.EASY));
+        final int roundNumber =  RandomUtils.getRandomInteger(maxRandomRoundIntegerNumber);
+        int result = baseNumber + roundNumber;
+        result = result < RoomFactoryImpl.MIN_ENEMIES_PER_ROOM 
+                ? RoomFactoryImpl.MIN_ENEMIES_PER_ROOM 
+                : result;
+        result = result > effectiveNumberOfRooms * RoomFactoryImpl.MAX_ENEMIES_PER_ROOM
+                ? effectiveNumberOfRooms * RoomFactoryImpl.MAX_ENEMIES_PER_ROOM
+                : result;
+        return result;
     }
 
-    private int getRandomNumberOfTreasure() {
-        return (int) Math.round(RandomUtils.getRandomGaussianNumber(this.getFloorMultiplierValue(), this.getFloorMultiplierValue())
-                * MULTIPLIER.get(this.difficulty.get()));
+    private int getRandomNumberOfTreasure(final int effectiveNumberOfRooms, final Difficulty difficulty, final int interagibleSetted) {
+        final int baseValue = (int) Math.round(effectiveNumberOfRooms * MULTIPLIER.get(Difficulty.NORMAL));
+        final int baseNumber = RandomUtils.getRandomIntegerGaussianNumber(baseValue, baseValue);
+        int result = (int) Math.round(baseNumber * MULTIPLIER.get(difficulty));
+        result = result < RoomFactoryImpl.MIN_INTERACTABLE_ACTION_PER_ROOM 
+                ? RoomFactoryImpl.MIN_INTERACTABLE_ACTION_PER_ROOM 
+                : result;
+        result = result > (effectiveNumberOfRooms * RoomFactoryImpl.MAX_INTERACTABLE_ACTIONS_PER_ROOM) - interagibleSetted
+                ? (effectiveNumberOfRooms * RoomFactoryImpl.MAX_INTERACTABLE_ACTIONS_PER_ROOM) - interagibleSetted
+                : result;
+        return result;
     }
 
-    private int getRandomNumberOfContraptions() {
-        return (int) Math.round(RandomUtils.getRandomGaussianNumber(this.getFloorMultiplierValue(), this.getFloorMultiplierValue())
-                        * MULTIPLIER.get(this.difficulty.get()));
-    }
-
-    private int getFloorMultiplierValue() {
-        return (int) Math.round(this.effectiveNumberOfRooms.get() * MULTIPLIER.get(Difficulty.NORMAL));
+    private int getRandomNumberOfContraptions(final int effectiveNumberOfRooms, final Difficulty difficulty, final int interagibleSetted) {
+        return this.getRandomNumberOfTreasure(effectiveNumberOfRooms, difficulty, interagibleSetted);
     }
 
 }
