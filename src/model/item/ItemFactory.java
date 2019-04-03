5ecd4e1edebf49fa4d8f5ba7ca291048e22aaee9
Alex Speranza <alex.speranza@studio.unibo.it>
2019-04-03 09:57:43 +0200

    ItemFactory: add skeleton of random item creation

--- src/model/item/ItemFactory.java
+++ src/model/item/ItemFactory.java
@@ -1,20 +1,29 @@
 package model.item;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
+import model.character.Statistic;
+import model.item.equipableitem.EquipableItem;
 import model.item.equipableitem.EquipableItemSword;
 import model.item.usableitem.UsableItemBomb;
 import model.item.usableitem.UsableItemPotion;
+import utils.randomcollection.RandomList;
+import utils.randomcollection.RandomListImpl;
+import utils.randomcollection.weightedItem.WeightedItemImpl;
 
 /**
  * Factory for generating random items from a database.
  */
 public final class ItemFactory {
 
-    private static final List<Item> DATABASE = new ArrayList<>();
+    private static final List<Function<ItemRarity, Item>> DATABASE = new ArrayList<>();
     private static final Random RNGENERATOR = new Random();
+    private static final RandomList<ItemRarity> RARITY_LIST = new RandomListImpl<>();
 
     static {
         initDatabase();
@@ -27,17 +36,38 @@ public final class ItemFactory {
      *  Initialize the Item database. 
      */
     public static void initDatabase() {
-        DATABASE.add(new UsableItemPotion());
-        DATABASE.add(new UsableItemBomb());
-        DATABASE.add(new EquipableItemSword());
+        Arrays.asList(ItemRarityImpl.values()).stream().forEach(v -> RARITY_LIST.add(new WeightedItemImpl<>(v,  v.getBaseWeight())));
+
+        DATABASE.add(UsableItemBomb::getNewInstance);
+        DATABASE.add(UsableItemPotion::getNewInstance);
+        DATABASE.add(EquipableItemSword::getNewInstance);
     }
 
     /**
+     * Return a new {@link model.item.Item}. If an {@link model.item.equipableitem.EquipableItem}
+     * is extracted, then additional {@link model.combat.action.Action} and {@link model.combat.action.effect.ActionEffect}
+     * are added as well, accordingly with the rarity of the item; otherwise the new item is returned without additions.
      * 
      * @return
      *          a random item from the database
      */
     public static Item getRandomItem() {
-        return DATABASE.get(RNGENERATOR.nextInt(DATABASE.size())).copy();
+        Item newItem = DATABASE.get(RNGENERATOR.nextInt(DATABASE.size())).apply(RARITY_LIST.getNext());
+
+        if (newItem.isEquipable()) {
+            EquipableItem eItem = ((EquipableItem) newItem);
+            final int maxNumOfModifiers = eItem.getRarityModifiers().get(eItem.getRarity()).getLeft() 
+                                           //weapons have the base damage modifier
+                                          + (eItem.getType().isWeapon() ? 1 : 0);
+            final int maxNumOfActions = eItem.getRarityModifiers().get(eItem.getRarity()).getRight();
+            for (int i = 0; i < maxNumOfModifiers; i++) {
+                //add effects
+            }
+            for (int i = 0; i < maxNumOfActions; i++) {
+                //add actions
+            }
+        }
+        return newItem;
+
     }
 }
