24291085f6d6a395abfd880a6b421b1afd357b49
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-04-04 23:17:22 +0200

    TargetType enumeration: improve documentation

--- src/model/combat/action/TargetType.java
+++ src/model/combat/action/TargetType.java
@@ -5,9 +5,23 @@ package model.combat.action;
  */
 public enum TargetType {
 
+    /**
+     * The associated {@link Action} can only target {@link ActionActor}
+     * present in the actor's own party.
+     */
     ALLY,
+    /**
+     * The associated {@link Action} can only target {@link ActionActor}
+     * present in the actor's opposing party.
+     */
     FOE,
+    /**
+     * The associated {@link Action} can target every {@link ActionActor}.
+     */
     EVERYONE,
+    /**
+     * The associated {@link Action} can only target it's source.
+     */
     SELF;
 
 }
