67602ee0f8684826f28327d64a4153ffade88084
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-04-05 01:24:18 +0200

    DamageEffect class: implement changes to interface, improve code quality

--- src/model/combat/action/effect/DamageEffect.java
+++ src/model/combat/action/effect/DamageEffect.java
@@ -1,71 +1,82 @@
 package model.combat.action.effect;
 
-
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import model.combat.actor.ActionActor;
 
 
 /**
- *  TO BE GENERALIZED AS AN INTERFACE
- *
+ * An {@link ActionEffect} which deals damage to a Character.
  */
-public class DamageEffect extends AbstractActionEffect {  //to be subdivided into physical damage/magical damage/fire/holy etc...
+public class DamageEffect extends AbstractActionEffect {
 
     private final double baseDamage;
     private double damage;
+    private double dealtDamage;
 
+    /**
+     * @param baseAmount the base amount of damage dealt
+     */
     public DamageEffect(final double baseAmount) {
         super();
         baseDamage = baseAmount;
+        damage = baseAmount;
     }
 
+    /**
+     * If the target is a Character, reduces its' health value.
+     */
     @Override
-    public void apply(final ActionActor target) {
+    public final void apply(final ActionActor target) {
+        dealtDamage = damage;
         //if target is Character -> target.modifyHealth(amount)
-        damage += baseDamage;
-    }
-
-    //GetDamagePreview ?
-    //GetEffectPreview -> "Deals (average) #damage" / "Inflicts status.getName() status" / .. (one for each effect)
-
-    @Override
-    public void updateEffectByTarget(final ActionActor target) {
-        super.updateEffectByTarget(target);
-        // TODO Auto-generated method stub
-        //foreach modifier attribute of the character, attr.modifyValue(damage)
+        //Do stuff
+        damage = baseDamage;
     }
 
     @Override
-    public void updateEffectBySource(final ActionActor source) {
-        super.updateEffectBySource(source);
-        // TODO Auto-generated method stub
-        //Override default implementation (but use it!)
-        //add character strenght etc.. to damage (if tag physical, else use faith/willpower)
+    public final String getLogMessage() {
+        return "Dealt " + dealtDamage + " HP damage ";
     }
 
     @Override
-    public String getLogMessage() {
-        return "Dealt " + damage + " HP damage ";
+    public final String getPreviewMessage() {
+        getSource().ifPresent(this::updateEffectBySource);
+        getTarget().ifPresent(this::updateEffectByTarget);
+        final String result =  "Deals " + damage + " HP damage ";
+        damage = baseDamage;
+        return result;
     }
 
-    public void setDamage(final double newDamage) {
-        damage = newDamage;
-    }
-
+    /**
+     * Adds a value to the damage that is going to be dealt.
+     * @param damage the amount to sum to the current damage
+     */
     public void addToDamage(final double damage) {
         this.damage += damage;
     }
 
-    //Maybe turn DamageEffect into interface and implement it here
+    /**
+     * Gets the base damage value.
+     * @return the base damage
+     */
     public double getBaseDamage() {
         return baseDamage;
     }
 
+    /**
+     * Gets the current damage value.
+     * @return the current damage
+     */
     public double getDamage() {
         return damage;
     }
 
+    /**
+     * Gets a description of the effect.
+     * @return a description of the effect
+     */
     @Override
     public String getDescription() {
         //This appends all the effect tags at the end of the description, maybe it would be better to
@@ -74,10 +85,15 @@ public class DamageEffect extends AbstractActionEffect {  //to be subdivided int
                                                             .map(t -> t.getLiteral()).collect(Collectors.joining(", "));
     }
 
+    /**
+     * Calls the parent implementation and
+     * confronts the base and current damage.
+     * @return true if the two entities are equal
+     */
     @Override
-    public boolean equals(final Object other) {
-        if (super.equals(other)) {
-            final DamageEffect o = ((DamageEffect) other);
+    public final boolean equals(final Object other) {
+        if (super.equals(other) && other instanceof DamageEffect) {
+            final DamageEffect o = (DamageEffect) other;
             return getBaseDamage() == o.getBaseDamage()
                     && getDamage() == o.getDamage();
         }
@@ -85,7 +101,7 @@ public class DamageEffect extends AbstractActionEffect {  //to be subdivided int
     }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
+    public final int hashCode() {
+        return Objects.hash(super.hashCode(), getBaseDamage());
     }
 }
