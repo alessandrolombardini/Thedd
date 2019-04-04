b2043563fab0434affa1832cae3956d1fc3ac61f
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-04-05 01:07:04 +0200

    ActionActor interface: 	-Rename addAction method to addActionToAvailable 	-Rename removeAction method to removeActionFromAvailable 	-Remove set/getPlaceInRound methods 	-Add Taggable extension 	-Add addActionToQueue, insertActionIntoQueue, removeActionFromQueue, getActionQueue, getNextQueuedAction, addStatus, getStatuses, removeStatus, getSelectedAction and resetSelectedAction methods 	-Split add/removeModifier methods into add/removeAction/EffectModifier methods

--- src/model/combat/actor/ActionActor.java
+++ src/model/combat/actor/ActionActor.java
@@ -3,19 +3,19 @@ package model.combat.actor;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-
 import model.combat.action.Action;
-import model.combat.logic.CombatLogic;
+import model.combat.action.effect.ActionEffect;
 import model.combat.modifier.Modifier;
-import model.combat.tag.Tag;
+import model.combat.status.Status;
+import model.combat.tag.Taggable;
 
 /**
  * 
  * An entity capable of performing Actions in and out of combat.<br>
- * It also holds collections of Statuses, {@link Modifier} and {@link Tag}.
+ * It also holds collections of {@link Status}, {@link Modifier} and {@link Tag}.
  * 
  */
-public interface ActionActor {
+public interface ActionActor extends Taggable {
 
     /**
      * Returns the literal name of the actor.
@@ -24,22 +24,46 @@ public interface ActionActor {
     String getName();
 
     /**
+     * Sets the {@link Action} that the actor is going to actively execute.
+     * @param action the next action of the actor
+     * @param selectedByActor set to true if the action was actively selected by the actor
+     */
+    void addActionToQueue(Action action, boolean selectedByActor);
+
+    /**
+     * Inserts an {@link Action} in the specified position of the
+     * actor's action queue.
+     * @param pos the position of the newly inserted action
+     * @param action the action to be inserted
+     * @param selectedByActor set to true if the action was 
+     *        actively selected by the actor 
+     */
+    void insertActionIntoQueue(int pos, Action action, boolean selectedByActor);
+
+    /**
+     * Removes the {@link Action} from the actor's queue.
+     * If the target action was the one selected by the actor, the value is reseted.
+     * @param action to be removed
+     */
+    void removeActionFromQueue(Action action);
+
+    /**
      * Returns an Optional containing the {@link Action} that the actor is going to actively execute.
      * @return the current action, Optional.empty() if no action is present
      */
-    Optional<Action> getAction();
+    Optional<Action> getNextQueuedAction();
 
     /**
-     * Sets the {@link Action} that the actor is going to actively execute.
-     * @param action the next action of the actor
+     * Gets the list of queued Actions.
+     * @return the list of the actor's queued actions
      */
-    void setAction(Action action);
+    List<Action> getActionQueue();
 
     /**
      * Adds the specified {@link Action} to the available set of Actions of the actor.
      * @param action the action to be added
      */
-    void addAction(Action action);
+    void addActionToAvailable(Action action);
 
     /**
      * Removes, if present, the specified {@link Action} from the actor's collection
@@ -47,37 +71,32 @@ public interface ActionActor {
      * @param action the action to be removed
      * @return true if the action was removed, false otherwise.
      */
-    boolean removeAction(Action action);
-
-    /**
-     * Override of default compareTo method.
-     * <p>
-     * Compares two actors by using their priority.<br>
-     * A positive integer is returned if this actor has higher priority
-     * than the other one's, negative if lower and 0 if the actors have same priority
-     * @param other the other Actors
-     * @return the result of the operation
-     */
-    int compareTo(ActionActor other);
-
-    /**
-     * Returns the actor's priority.
-     * @return the actor's priority
-     */
-    int getPriority();
+    boolean removeActionFromAvailable(Action action);
 
     /**
      * Sets what {@link Action} the actor is capable of executing.
      * <p>
      * @param actions the list of available actions
      */
-    void setAvailableActions(Set<? extends Action> actions);
+    void setAvailableActions(Set<Action> actions);
 
     /**
-     * Returns the list of available {@link Action}.
+     * Gets a list containing the available actions of the actor.
      * @return the list of actions
      */
-    //List<Action> getAvailableActions();
+    List<Action> getAvailableActionsList();
+
+    /**
+     * Gets a set containing the available actions of the actor.
+     * @return the set of actions
+     */
+    Set<Action> getAvailableActionsSet();
+
+    /**
+     * Returns the actor's priority.
+     * @return the actor's priority
+     */
+    int getPriority();
 
     /**
      * Sets whether or not the actor is in combat.
@@ -92,88 +111,76 @@ public interface ActionActor {
     boolean isInCombat();
 
     /**
-     * If the actor is in combat, it returns his place (starting from 1) 
-     * in the current turn queue, as set by the {@link CombatLogic}.
-     * Otherwise, it returns 0.
-     * @return the index of the character in the current turn queue
-     */
-    int getPlaceInRound();
-
-    /**
-     * If the actor is in combat, sets the value to be returned by {@link ActionActor.getPlaceInRound}
-     * method.
-     * @param place an integer greater than 0
-     */
-    void setPlaceInRound(int place);
-    //compareTo, se la source non è un interactable, compara l'agilità dei due personaggi. Se source è un interactable, ha default priorità minore
-    //alternativa: ActionActor implementa metodo per ritornare una priorità (interactable ritornerà -1, personaggi la loro statistica, effetti duraturi una loro iniziativa (oppure -1 e gli interactable -2))
-
-    /**
-     * Resets the value to be shown by getPlaceInRound method.
-     */
-    void resetPlaceInRound();
-
-    /**
-     * Gets the Set of {@link Tag} of the Actor.
-     * @return a Set of Tags 
-     */
-    Set<Tag> getTags();
-
-    /**
-     * Adds one or more {@link Tag} to the Actor's Set.
-     * @param tags the Set of tags to be added
-     * @param arePermanent true if the tags are going to be not removable, false otherwise
-     */
-    void addTags(Set<Tag> tags, boolean arePermanent);
-
-    /**
-     * Adds a {@link Tag} to the Actor's Set.
-     * @param isPermanent true if the tag is going to be not removable, false otherwise
-     * @param tag the tag to be added
-     */
-    void addTag(Tag tag, boolean isPermanent);
-
-    /**
-     * Removes a {@link Tag} to the Actor's Set, if the the provided tag is not permanent.
-     * @param tag the tag to be removed
-     * @return true if the tag was removed, false otherwise
-     */
-    boolean removeTag(Tag tag);
-
-    /**
-     * Adds a {@link Modifier} to the actor.
-     * @param modifier the modifier to be added
-     * @param isPermanent true if the modifier is going to be not removable, false otherwise
-     */
-    void addModifier(Modifier modifier, boolean isPermanent);
-
-    /**
-     * Removes a {@link Modifier} to the actor, if it's not permanent.
-     * @param modifier the modifier to be added
-     */
-    void removeModifier(Modifier modifier);
-
-    /**
      * Gets the Set of {@link Modifier} applicable to {@link Action}.
      * @return the set of action modifiers
      */
-    Set<Modifier> getActionModifiers();
+    Set<Modifier<Action>> getActionModifiers();
 
     /**
      * Gets the Set of {@link Modifier} applicable to {@link ActionEffect.
      * @return the set of effect modifiers
      */
-    Set<Modifier> getEffectModifiers();
+    Set<Modifier<ActionEffect>> getEffectModifiers();
 
     /**
-     * Gets a list containing the available actions of the actor.
-     * @return the list of actions
+     * Gets a collection of {@link Status} assigned to the actor.
+     * @return the statuses of the actor
      */
-    List<Action> getAvailableActionsList();
+    List<Status> getStatuses();
 
     /**
-     * Gets a set containing the available actions of the actor.
-     * @return the set of actions
+     * Adds a {@link Status} to the actor.<br>
+     * It also adds the various {@link Tag} provided by
+     * the status to the actor.
+     * @param status the status to be added
      */
-    Set<Action> getAvailableActionsSet();
+    void addStatus(Status status);
+
+    /**
+     * Removes a {@link Status} from the actor.<br>
+     * It also removes the various {@link Tag} provided by
+     * the status to the actor.
+     * @param status the status to be removed
+     */
+    void removeStatus(Status status);
+
+    /**
+     * Gets the currently selected {@link Action}.
+     * @return the action selected by the actor
+     */
+    Optional<Action> getSelectedAction();
+
+    /**
+     * Resets the selected action of the actor.
+     */
+    void resetSelectedAction();
+
+    /**
+     * Adds a {@link Modifier} affecting {@link ActionEffect} entities to the actor.
+     * @param modifier the modifier to be added
+     * @param isPermanent true if the modifier is going to be not removable, false otherwise
+     */
+    void addEffectModifier(Modifier<ActionEffect> modifier, boolean isPermanent);
+
+    /**
+     * Removes a non permanent {@link Modifier} affecting {@link ActionEffect}
+     *  entities from the actor.
+     * @param modifier the modifier to be removed
+     */
+    void removeEffectModifier(Modifier<ActionEffect> modifier);
+
+    /**
+     * Adds a {@link Modifier} affecting {@link Action} entities to the actor.
+     * @param modifier the modifier to be added
+     * @param isPermanent true if the modifier is going to be not removable, false otherwise
+     */
+    void addActionModifier(Modifier<Action> modifier, boolean isPermanent);
+
+    /**
+     * Removes a non permanent {@link Modifier} affecting {@link Action} entities 
+     * from the actor.
+     * @param modifier the modifier to be removed
+     */
+    void removeActionModifier(Modifier<Action> modifier);
+
 }
