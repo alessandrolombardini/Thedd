8747d14da1ce25bec971b5a889fa1f4cc9e5eae8
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-04-05 00:34:53 +0200

    Add HitChanceModifier class

---
+++ src/model/combat/modifier/HitChanceModifier.java
@@ -0,0 +1,31 @@
+package model.combat.modifier;
+
+import model.combat.action.Action;
+
+/**
+ * A modifier that modifies an {@link Action}'s hit chance. 
+ */
+public class HitChanceModifier extends AbstractModifier<Action> {
+
+    /**
+     * @param value the value that will be applied to the modifiable
+     * @param isPercentage true if the value shall be treated as a percentage
+     * @param type declares whether this modifier should be applied on attack, defense or every time
+     */
+    public HitChanceModifier(final double value, final boolean isPercentage, final ModifierActivation type) {
+        super(value, isPercentage, type);
+    }
+
+    /**
+     * Modifies the hit chances of the target Action as follows:<p>
+     * modifier = isPercentage ? baseHitChance * value : value;<br>
+     * action.addToCurrentHitChance(modifier);
+     */
+    @Override
+    public void modify(final Action modifiable) {
+        final Action action = (Action) modifiable;
+        final double modifier = isPercentage() ? action.getBaseHitChance() * getValue() : getValue();
+        action.addToCurrentHitChance(modifier);
+    }
+
+}
