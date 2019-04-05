95ff5320c5c6a1f5e41b4d88f0c42a9955a444a1
Alessandro Lombardini <alessandr.lombardin3@studio.unibo.it>
2019-04-05 20:09:33 +0200

    Change the method to create a new game inside the view controller PlayMenuController

--- src/thedd/view/controller/PlayMenuController.java
+++ src/thedd/view/controller/PlayMenuController.java
@@ -3,7 +3,7 @@ package thedd.view.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import thedd.view.ApplicationState;
+import thedd.view.ApplicationViewState;
 import thedd.view.SubViewControllerImpl;
 
 /**
@@ -13,8 +13,7 @@ public class PlayMenuController extends SubViewControllerImpl {
 
     private static final String ERROR_TITLE_INPUTERROR = "Input error";
     private static final String ERROR_TEXT_NULL = "Nope, you have to insert number of rooms and floors";
-    private static final String ERROR_TEXT_NONVALIDVALUE = "Nope, values of rooms and floors not acceptable";
-    private static final ApplicationState NEXT_SCENE = ApplicationState.MENU;
+    private static final String ERROR_TEXT_NONVALIDVALUE = "Nope, the number of rooms and/or floors is not acceptable";
 
     @FXML
     private TextField playerNameField;
@@ -36,15 +35,12 @@ public class PlayMenuController extends SubViewControllerImpl {
     protected void handlePlayButtonAction() {
         if (this.numberOfRoomsField.getText().isEmpty() || this.numberOfFloorsField.getText().isEmpty()) {
             this.getDialogFactory().createErrorDialog(ERROR_TITLE_INPUTERROR, ERROR_TEXT_NULL).show();
-            return;
-        }
-        final boolean isGoodValues = this.getController().newGame(this.playerNameField.getText(),
-                                                                  this.numberOfFloorsField.getText(), 
-                                                                  this.numberOfFloorsField.getText());
-        if (!isGoodValues) {
+        } else if (!this.getController().newGame(this.playerNameField.getText(),
+                                                 this.numberOfFloorsField.getText(), 
+                                                 this.numberOfFloorsField.getText())) {
             this.getDialogFactory().createErrorDialog(ERROR_TITLE_INPUTERROR, ERROR_TEXT_NONVALIDVALUE).show();
-            return;
+        } else {
+            this.getView().setView(ApplicationViewState.GAME_OVER);
         }
-        this.getView().setView(NEXT_SCENE);
     }
 }
