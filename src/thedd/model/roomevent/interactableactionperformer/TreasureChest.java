671614d128a02c47dfb6cdad6c201f6debbb5fa8
Alex Speranza <alex.speranza@studio.unibo.it>
2019-04-05 18:23:21 +0200

    Refactor imports and contructor accordingly to changes of Action

--- src/thedd/model/roomevent/interactableactionperformer/TreasureChest.java
+++ src/thedd/model/roomevent/interactableactionperformer/TreasureChest.java
@@ -1,11 +1,12 @@
 package thedd.model.roomevent.interactableactionperformer;
 
-import java.util.Arrays;
-
-import model.combat.enums.TargetType;
-import model.combat.implementations.AbstractAction;
-import model.combat.interfaces.Action;
+import thedd.model.combat.action.Action;
+import thedd.model.combat.action.ActionCategory;
+import thedd.model.combat.action.ActionImpl;
+import thedd.model.combat.action.LogMessageType;
+import thedd.model.combat.action.TargetType;
 import thedd.model.combat.action.effect.ItemGiverEffect;
+import thedd.model.combat.action.targeting.DefaultTargeting;
 
 /**
  * Specialization of {@link thedd.model.roomevent.interactableactionperformer.InteractableActionPerformer}.
@@ -14,7 +15,15 @@ import thedd.model.combat.action.effect.ItemGiverEffect;
 public class TreasureChest extends AbstractInteractableActionPerformer implements InteractableActionPerformer {
 
     private static final String NAME = "Treasure Chest";
-    private static final Action ACTION = new AbstractAction(null, NAME, Arrays.asList(new ItemGiverEffect()), 1, TargetType.EVERYONE) { };
+    private static final Action ACTION;
+    static {
+        final String description = "An item has been found";
+        ACTION = new ActionImpl(NAME, ActionCategory.INTERACTABLE, 
+                                new DefaultTargeting(), 1.0, 
+                                TargetType.EVERYONE, description, 
+                                LogMessageType.TREASURE_ACTION);
+        ACTION.addEffect(new ItemGiverEffect());
+    }
 
     /**
      * 
