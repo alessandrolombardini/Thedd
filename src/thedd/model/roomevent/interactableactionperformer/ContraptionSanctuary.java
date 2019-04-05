cab4cbc250f5dd16727194698589eac6f6b59d78
Alex Speranza <alex.speranza@studio.unibo.it>
2019-04-05 18:25:54 +0200

    Refactor imports and constructor accordingly to Action changes. Healing effect is yet to be added to the action

--- src/thedd/model/roomevent/interactableactionperformer/ContraptionSanctuary.java
+++ src/thedd/model/roomevent/interactableactionperformer/ContraptionSanctuary.java
@@ -1,11 +1,11 @@
 package thedd.model.roomevent.interactableactionperformer;
 
-import java.util.ArrayList;
-
-import model.combat.enums.TargetType;
-import model.combat.implementations.AbstractAction;
-import model.combat.interfaces.Action;
-import model.item.HealingEffect;
+import thedd.model.combat.action.Action;
+import thedd.model.combat.action.ActionCategory;
+import thedd.model.combat.action.ActionImpl;
+import thedd.model.combat.action.LogMessageType;
+import thedd.model.combat.action.TargetType;
+import thedd.model.combat.action.targeting.DefaultTargeting;
 
 /**
  * Specialization of {@link thedd.model.roomevent.interactableactionperformer.InteractableActionPerformer}.
@@ -14,14 +14,23 @@ import model.item.HealingEffect;
 public class ContraptionSanctuary extends AbstractInteractableActionPerformer implements Contraption {
 
     private static final String NAME = "Sanctuary";
-    private static final Action ACTION = new AbstractAction(null, "Sanctuary", new ArrayList<>(), 1.0, TargetType.EVERYONE) { };
+    private static final Action ACTION;
 
+    static {
+        final String description = "";
+        ACTION = new ActionImpl(NAME, ActionCategory.INTERACTABLE, 
+                                 new DefaultTargeting(), 1.0, 
+                                 TargetType.EVERYONE, description, 
+                                 LogMessageType.CONTRAPTION_ACTION);
+        //TODO ACTION.addEffect(new HealingEffect(1.0));
+        
+    }
+    
     /**
      * Create a new Sanctuary and set his Action to heal everybody in the target's party.
      */
     public ContraptionSanctuary() {
         super(NAME, ACTION);
-        ACTION.addEffect(new HealingEffect(Double.POSITIVE_INFINITY));
     }
 
     /**
