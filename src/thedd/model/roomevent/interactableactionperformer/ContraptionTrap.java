7551c4b696c5d8521270431fe5facd3fd5ec0ef9
Alex Speranza <alex.speranza@studio.unibo.it>
2019-04-05 18:24:35 +0200

    Refactor imports and constructor accordingly to Action changes

--- src/thedd/model/roomevent/interactableactionperformer/ContraptionTrap.java
+++ src/thedd/model/roomevent/interactableactionperformer/ContraptionTrap.java
@@ -1,11 +1,13 @@
 package thedd.model.roomevent.interactableactionperformer;
 
-import java.util.ArrayList;
+import thedd.model.combat.action.Action;
+import thedd.model.combat.action.ActionCategory;
+import thedd.model.combat.action.ActionImpl;
+import thedd.model.combat.action.LogMessageType;
+import thedd.model.combat.action.TargetType;
+import thedd.model.combat.action.effect.DamageEffect;
+import thedd.model.combat.action.targeting.DefaultTargeting;
 
-import model.combat.enums.TargetType;
-import model.combat.implementations.AbstractAction;
-import model.combat.implementations.DamageEffect;
-import model.combat.interfaces.Action;
 
 /**
  * Specialization if {@link thedd.model.roomevent.interactableactionperformer.InteractableActionPerformer}.
@@ -14,15 +16,23 @@ import model.combat.interfaces.Action;
 public class ContraptionTrap extends AbstractInteractableActionPerformer implements Contraption {
 
     private static final String NAME = "Trap";
-    private static final Action ACTION = new AbstractAction(null, NAME, new ArrayList<>(), 1.0, TargetType.EVERYONE) { }; 
+    private static final Action ACTION;
     private static final double BASE_DAMAGE = 20.0;
+
+    static {
+        final String description = "";
+        ACTION = new ActionImpl(NAME, ActionCategory.INTERACTABLE,
+                                 new DefaultTargeting(), 1.0, 
+                                 TargetType.EVERYONE, description,
+                                 LogMessageType.CONTRAPTION_ACTION);
+        ACTION.addEffect(new DamageEffect(BASE_DAMAGE));
+    }
 
     /**
      * Create a new Trap and set the action.
      */
     public ContraptionTrap() {
         super(NAME, ACTION);
-        this.getAction().get().addEffect(new DamageEffect(BASE_DAMAGE));
     }
 
     /**
