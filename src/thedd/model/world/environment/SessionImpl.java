2ec86c43e41076ff214382ba60e73f1b6ac6976b
Alessandro Lombardini <alessandr.lombardin3@studio.unibo.it>
2019-04-08 19:47:05 +0200

    Add some checks to SessionBuilder params setter

--- src/thedd/model/world/environment/SessionImpl.java
+++ src/thedd/model/world/environment/SessionImpl.java
@@ -8,7 +8,7 @@ import thedd.model.character.CharacterFactory;
 import thedd.model.world.Difficulty;
 
 /**
- * Implementation of {@link thedd.model.world.Session}.
+ * Implementation of {@link thedd.model.world.environment.Session}.
  */
 public final class SessionImpl implements Session {
 
@@ -18,12 +18,9 @@ public final class SessionImpl implements Session {
     /**
      * Session' constructor.
      * 
-     * @param playerCharacterName 
-     *          name of the player character
-     * @param numberOfLevels 
-     *          number of levels of game's map
-     * @param numberOfRooms 
-     *          number of rooms of each floor of the map
+     * @param playerCharacterName name of the player character
+     * @param numberOfLevels      number of levels of game's map
+     * @param numberOfRooms       number of rooms of each floor of the map
      */
     private SessionImpl(final Optional<String> playerCharacterName, final int numberOfLevels, final int numberOfRooms) {
         Objects.requireNonNull(playerCharacterName);
@@ -52,7 +49,7 @@ public final class SessionImpl implements Session {
      */
     @Override
     public String toString() {
-        return "SessionImpl [environment=" + environment + ", playerCharacter=" + playerCharacter + "]";
+        return "SessionImpl [environment=" + this.environment + ", playerCharacter=" + this.playerCharacter + "]";
     }
 
     /**
@@ -60,6 +57,8 @@ public final class SessionImpl implements Session {
      *
      */
     public static class SessionBuilder {
+
+        private static final String ERROR_PARAMSNOTVALID = "Not all values has been setted correctly";
 
         private Optional<String> playerCharacterName;
         private Optional<Integer> numberOfRooms;
@@ -79,8 +78,7 @@ public final class SessionImpl implements Session {
         /**
          * This method allows to set the name of the player character.
          * 
-         * @param name 
-         *              name of the player character
+         * @param name name of the player character
          * @return the builder
          */
         public SessionBuilder playerCharacterName(final String name) {
@@ -91,24 +89,22 @@ public final class SessionImpl implements Session {
         /**
          * This method allows to set the number of rooms.
          * 
-         * @param num 
-         *              number of rooms
+         * @param num number of rooms
          * @return the builder
          */
         public SessionBuilder numberOfRooms(final int num) {
-            this.numberOfRooms = Optional.of(num);
+            this.numberOfRooms = Optional.of(num).filter(n -> n >= EnvironmentImpl.MIN_NUMBER_OF_ROOMS);
             return this;
         }
 
         /**
          * This method allows to set the number of floors.
          * 
-         * @param num 
-         *              number of floors
+         * @param num number of floors
          * @return the builder
          */
         public SessionBuilder numberOfFloors(final int num) {
-            this.numberOfFloors = Optional.of(num);
+            this.numberOfFloors = Optional.of(num).filter(n -> n >= EnvironmentImpl.MIN_NUMBER_OF_FLOORS);
             return this;
         }
 
@@ -120,7 +116,7 @@ public final class SessionImpl implements Session {
          */
         public SessionImpl build() throws IllegalStateException {
             if (!this.numberOfRooms.isPresent() || !this.numberOfFloors.isPresent() || this.built) {
-                throw new IllegalStateException();
+                throw new IllegalStateException(ERROR_PARAMSNOTVALID);
             }
             this.built = true;
             return new SessionImpl(this.playerCharacterName, this.numberOfFloors.get(), this.numberOfRooms.get());
