160c9a1eaaf20bcab5965ad4e2084b890289b266
Alessandro Lombardini <alessandr.lombardin3@studio.unibo.it>
2019-04-09 20:23:24 +0200

    Remove builder by SessionImpl

--- src/thedd/model/world/environment/SessionImpl.java
+++ src/thedd/model/world/environment/SessionImpl.java
@@ -12,6 +12,8 @@ import thedd.model.world.Difficulty;
  */
 public final class SessionImpl implements Session {
 
+    private static final String ERROR_PARAMSNOTVALID = "Incorrect number of levels or rooms, under the minimum";
+
     private final Environment environment;
     private final BasicCharacter playerCharacter;
 
@@ -19,13 +21,16 @@ public final class SessionImpl implements Session {
      * Session' constructor.
      * 
      * @param playerCharacterName name of the player character
-     * @param numberOfLevels      number of levels of game's map
-     * @param numberOfRooms       number of rooms of each floor of the map
+     * @param numOfLevels         number of levels of map
+     * @param numOfRooms          number of rooms of each floor of the map
      */
-    private SessionImpl(final Optional<String> playerCharacterName, final int numberOfLevels, final int numberOfRooms) {
+    public SessionImpl(final Optional<String> playerCharacterName, final int numOfLevels, final int numOfRooms) {
         Objects.requireNonNull(playerCharacterName);
+        if (numOfLevels < EnvironmentImpl.MIN_NUMBER_OF_FLOORS || numOfRooms < EnvironmentImpl.MIN_NUMBER_OF_ROOMS) {
+            throw new IllegalArgumentException(ERROR_PARAMSNOTVALID);
+        }
         this.playerCharacter = CharacterFactory.createPlayerCharacter(playerCharacterName, Difficulty.NORMAL);
-        this.environment = new EnvironmentImpl(numberOfLevels, numberOfRooms);
+        this.environment = new EnvironmentImpl(numOfLevels, numOfRooms);
     }
 
     /**
@@ -44,84 +49,8 @@ public final class SessionImpl implements Session {
         return this.environment;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public String toString() {
         return "SessionImpl [environment=" + this.environment + ", playerCharacter=" + this.playerCharacter + "]";
     }
-
-    /**
-     * Builder of Session.
-     *
-     */
-    public static class SessionBuilder {
-
-        private static final String ERROR_PARAMSNOTVALID = "Not all values has been setted correctly";
-
-        private Optional<String> playerCharacterName;
-        private Optional<Integer> numberOfRooms;
-        private Optional<Integer> numberOfFloors;
-        private boolean built;
-
-        /**
-         * Builder' constructor.
-         */
-        public SessionBuilder() {
-            this.playerCharacterName = Optional.empty();
-            this.numberOfRooms = Optional.empty();
-            this.numberOfFloors = Optional.empty();
-            this.built = false;
-        }
-
-        /**
-         * This method allows to set the name of the player character.
-         * 
-         * @param name name of the player character
-         * @return the builder
-         */
-        public SessionBuilder playerCharacterName(final String name) {
-            this.playerCharacterName = Optional.ofNullable(name);
-            return this;
-        }
-
-        /**
-         * This method allows to set the number of rooms.
-         * 
-         * @param num number of rooms
-         * @return the builder
-         */
-        public SessionBuilder numberOfRooms(final int num) {
-            this.numberOfRooms = Optional.of(num).filter(n -> n >= EnvironmentImpl.MIN_NUMBER_OF_ROOMS);
-            return this;
-        }
-
-        /**
-         * This method allows to set the number of floors.
-         * 
-         * @param num number of floors
-         * @return the builder
-         */
-        public SessionBuilder numberOfFloors(final int num) {
-            this.numberOfFloors = Optional.of(num).filter(n -> n >= EnvironmentImpl.MIN_NUMBER_OF_FLOORS);
-            return this;
-        }
-
-        /**
-         * This method build the session.
-         * 
-         * @return a SessionImpl
-         * @throws IllegalStateException if number of floors or rooms is not acceptable
-         */
-        public SessionImpl build() throws IllegalStateException {
-            if (!this.numberOfRooms.isPresent() || !this.numberOfFloors.isPresent() || this.built) {
-                throw new IllegalStateException(ERROR_PARAMSNOTVALID);
-            }
-            this.built = true;
-            return new SessionImpl(this.playerCharacterName, this.numberOfFloors.get(), this.numberOfRooms.get());
-        }
-
-    }
-
 }
