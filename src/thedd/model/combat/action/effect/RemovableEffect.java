71a7052a10620cbaac412264c1dd735200ff5909
Alex Speranza <alex.speranza@studio.unibo.it>
2019-04-10 08:46:40 +0200

    Renames removeBonus method to remove

--- src/thedd/model/combat/action/effect/RemovableEffect.java
+++ src/thedd/model/combat/action/effect/RemovableEffect.java
@@ -9,9 +9,9 @@ package thedd.model.combat.action.effect;
 public interface RemovableEffect extends ActionEffect {
 
     /**
-     * Remove the bonus applied before.
+     * Remove the effect applied before.
      * @throws IllegalStateException if the effect is not applied before
      */
-    void removeBonus();
+    void remove();
 
 }
