81f7188a2c628b967c5695a449f06ae018afd749
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2019-04-11 08:23:17 +0200

    Refactor of some errors in StatValuesImpl class

--- src/thedd/model/character/statistics/StatValuesImpl.java
+++ src/thedd/model/character/statistics/StatValuesImpl.java
@@ -7,32 +7,47 @@ public final class StatValuesImpl implements StatValues {
 
     private int actual;
     private int max;
+    /**
+     * Value used to not set the maximum field.
+     */
+    public static final int NO_MAX = -1;
 
     /**
      * StatValues constructor.
      * 
-     * @param value specifies the max value of this statistic. The actual value is
-     *              automatically set.
+     * @param actualValue specifies the actual value of this Statistic.
+     * @param maxValue    specifies the max value of this statistic.
+     * 
+     * @throws an IllegalArgumentException if values are negative.
      */
-    public StatValuesImpl(final int value) {
-        this.max = value;
-        this.actual = value;
+    public StatValuesImpl(final int actualValue, final int maxValue) {
+        if (maxValue != NO_MAX && (maxValue < 1 || actualValue < 1)) {
+            throw new IllegalArgumentException();
+        }
+        this.max = maxValue;
+        this.actual = actualValue;
     }
 
     @Override
     public void updateActual(final int value) {
-        if (this.actual + value > this.max) {
-            this.actual = this.max;
-        } else {
-            this.actual = this.actual + value;
+        if (this.actual + value > 0) {
+            if (this.max != NO_MAX) {
+                if (this.actual + value > this.max) {
+                    this.actual = this.max;
+                }
+            } else {
+                this.actual = this.actual + value;
+            }
         }
     }
 
     @Override
     public void updateMax(final int value) {
-        final int oldMax = this.max;
-        this.max = this.max + value;
-        updateActual((int) (this.actual * this.max) / oldMax);
+        if (this.max != NO_MAX) {
+            final int oldMax = this.max;
+            this.max = this.max + value;
+            updateActual((int) (this.actual * this.max) / oldMax);
+        }
     }
 
     @Override
@@ -47,6 +62,9 @@ public final class StatValuesImpl implements StatValues {
 
     @Override
     public String toString() {
+        if (this.max == NO_MAX) {
+            return "" + this.actual;
+        }
         return this.actual + "/" + this.max;
     }
 }
