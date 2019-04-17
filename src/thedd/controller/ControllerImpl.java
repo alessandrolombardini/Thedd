b46497e8b92d93cffde748f592507c1cbc5ceb49
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-04-17 14:16:01 +0200

    ControllerImpl class: implement changes to the interface

--- src/thedd/controller/ControllerImpl.java
+++ src/thedd/controller/ControllerImpl.java
@@ -10,6 +10,16 @@ import thedd.controller.informations.StatisticsInformations;
 import thedd.controller.informations.StatisticsInformationsImpl;
 import thedd.model.Model;
 import thedd.model.ModelImpl;
+import thedd.model.combat.action.Action;
+import thedd.model.combat.action.result.ActionResult;
+import thedd.model.combat.actionexecutor.ActionExecutor;
+import thedd.model.combat.actionexecutor.OutOfCombatActionExecutor;
+import thedd.model.combat.actionexecutor.StatusUpdateActionExecutor;
+import thedd.model.combat.actor.ActionActor;
+import thedd.model.combat.encounter.HostileEncounter;
+import thedd.model.combat.instance.ActionExecutionInstance;
+import thedd.model.combat.instance.CombatStatus;
+import thedd.model.combat.instance.ExecutionInstanceImpl;
 import thedd.model.item.Item;
 import thedd.model.item.usableitem.UsableItem;
 import thedd.model.world.environment.EnvironmentImpl;
@@ -26,6 +36,7 @@ public class ControllerImpl implements Controller {
     private final Model model;
     private InventoryInformations inventoryInfo;
     private StatisticsInformations statisticsInfo;
+    private Optional<ActionExecutor> actionExecutor = Optional.empty();
 
     /**
      * Create a new Controller instance.
@@ -133,5 +144,138 @@ public class ControllerImpl implements Controller {
     @Override
     public StatisticsInformations getStatisticsInformations() {
         return this.statisticsInfo;
+    }
+
+    //Action execution and combat management session
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void undoActionSelection() {
+        final ActionActor playerActor = this.model.getSession().getPlayerCharacter();
+        playerActor.resetSelectedAction();
+        //call view to tell player to select a new action
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void targetSelected(final ActionActor target) {
+        final ActionActor playerActor = this.model.getSession().getPlayerCharacter();
+        final ActionExecutor currentExecutor = actionExecutor.get();
+        playerActor.getSelectedAction().ifPresent(a -> {
+            a.setTargets(target, a.getValidTargets(currentExecutor.getExecutionInstance()));
+            currentExecutor.addActorToQueue(playerActor);
+        });
+        if (currentExecutor.isRoundReady()) {
+            evaluateNextAction();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void executeSingleAction(final Action action) {
+        final ActionActor playerActor = this.model.getSession().getPlayerCharacter();
+        final ActionExecutionInstance instance = new ExecutionInstanceImpl();
+        actionExecutor = Optional.of(new OutOfCombatActionExecutor(action));
+        instance.addPlayerPartyMember(playerActor);
+        actionExecutor.get().setExecutionInstance(instance);
+        if (action.getTargets().isEmpty()) {
+            //call view to tell player to select a target
+        } else {
+            evaluateNextAction();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void executeCurrentAction() {
+        if (!actionExecutor.isPresent()) {
+            return;
+        }
+        final ActionExecutor executor = actionExecutor.get();
+        executor.executeCurrentAction();
+        //tell view to log        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void evaluateExecutionState() {
+        if (!actionExecutor.isPresent()) {
+            return;
+        }
+        final ActionExecutor executor = actionExecutor.get();
+        executor.updateExecutionStatus();
+        final CombatStatus status = executor.getExecutionStatus();
+        switch (status) {
+        case COMBAT_ENDED:
+            actionExecutor = Optional.empty();
+            break;
+        case PLAYER_LOST:
+            //TODO
+            break;
+        case PLAYER_WON:
+            //TODO
+            break;
+        case ROUND_IN_PROGRESS:
+            evaluateNextAction();
+            break;
+        case ROUND_PAUSED:
+            //tell view to select a new action
+            break;
+        default:
+            executor.prepareNextRound();
+            //view.showNextTurn ?
+            //tell view to select a new action
+            break;
+        }
+    }
+
+    private void startCombat(final HostileEncounter encounter) {
+        final ActionActor playerActor = this.model.getSession().getPlayerCharacter();
+        final ActionExecutionInstance instance = new ExecutionInstanceImpl();
+        final ActionExecutor combatExecutor = encounter.getCombatLogic();
+        instance.addPlayerPartyMember(playerActor);
+        instance.addNPCsPartyMembers(encounter.getNPCs());
+        combatExecutor.setExecutionInstance(instance);
+        combatExecutor.startExecutor();
+        actionExecutor = Optional.of(combatExecutor);
+        if (combatExecutor.isRoundReady()) {
+            evaluateNextAction();
+        } else {
+            //call view to tell player to select an action
+        }
+    }
+
+    private void evaluateNextAction() {
+        actionExecutor.ifPresent(a -> {
+            a.setNextAction();
+            final Optional<ActionResult> result = a.evaluateCurrentAction();
+            //view visualize(result)
+        });
+    }
+
+    private void updateStatuses() {
+        final ActionExecutor executor = new StatusUpdateActionExecutor();
+        final ActionExecutionInstance instance = new ExecutionInstanceImpl();
+        instance.addPlayerPartyMember(model.getSession().getPlayerCharacter());
+        executor.setExecutionInstance(instance);
+        executor.startExecutor();
+        actionExecutor = Optional.of(executor);
+        evaluateNextAction();
+    }
+
+    @Override
+    public void selectAction(final Action action) {
+        model.getSession().getPlayerCharacter().addActionToQueue(action.getCopy(), true);
+        //call view to tell player to select a target (even if the action target type is SELF?)
     }
 }
