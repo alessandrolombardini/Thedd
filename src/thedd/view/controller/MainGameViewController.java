d578976469e32b3e992556b4b8a5ac7aa1a494e9
Alessandro Lombardini <alessandr.lombardin3@studio.unibo.it>
2019-04-18 13:59:50 +0200

    Add two interface, GameView and ExplorationView, for SubViewControllers to call their methods

--- src/thedd/view/controller/MainGameViewController.java
+++ src/thedd/view/controller/MainGameViewController.java
@@ -1,17 +1,27 @@
 package thedd.view.controller;
 
-import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Optional;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import thedd.model.combat.action.Action;
+import thedd.model.combat.action.result.ActionResult;
+import thedd.model.combat.actor.ActionActor;
 import thedd.view.ViewNode;
+import thedd.view.controller.interfaces.ExplorationView;
+import thedd.view.controller.interfaces.GameView;
 import thedd.view.nodewrapper.ViewNodeWrapper;
 import thedd.view.nodewrapper.ViewNodeWrapperFactory;
 
 /**
  * View controller of game scene.
  */
-public class GameController extends ViewNodeControllerImpl {
+public class MainGameViewController extends ViewNodeControllerImpl implements GameView {
+
+    private enum Position {
+        UP, DOWN_SX, DOWN_DX;
+    };
 
     @FXML
     private AnchorPane inventoryContent;
@@ -22,36 +32,99 @@ public class GameController extends ViewNodeControllerImpl {
     @FXML
     private AnchorPane gameContent;
 
-    private static final ViewNode INIT_NODE_UP = ViewNode.INVENTORY;
-    private static final ViewNode INIT_NODE_DOWN_SX = ViewNode.INVENTORY;
+    private static final ViewNode INIT_NODE_UP = ViewNode.STATISTICS;
+    private static final ViewNode INIT_NODE_DOWN_SX = ViewNode.ACTION_SELECTOR;
     private static final ViewNode INIT_NODE_DOWN_DX = ViewNode.STATISTICS;
 
-    private final List<ViewNodeController> viewControllers;
+    private final EnumMap<Position, Optional<ViewNodeController>> nodeControllers;
 
     /**
      * GameController constructor.
      */
-    public GameController() {
+    public MainGameViewController() {
         super();
-        this.viewControllers = new ArrayList<>();
+        this.nodeControllers = new EnumMap<Position, Optional<ViewNodeController>>(Position.class);
+        this.nodeControllers.put(Position.UP, Optional.empty());
+        this.nodeControllers.put(Position.DOWN_SX, Optional.empty());
+        this.nodeControllers.put(Position.DOWN_DX, Optional.empty());
     }
 
     @Override
     public final void update() {
-        this.viewControllers.forEach(c -> c.update());
+        this.nodeControllers.values().stream().filter(c -> c.isPresent())
+                                              .forEach(c -> c.get().update());
     }
 
     @Override
     protected final void initView() {
-        ViewNodeWrapper node = ViewNodeWrapperFactory.createViewNodeWrapper(INIT_NODE_UP, this.getController(), 
-                                                                            this.getView());
-        this.gameContent.getChildren().add(new AnchorPane());
-        this.viewControllers.add(node.getController());
-        node = ViewNodeWrapperFactory.createViewNodeWrapper(INIT_NODE_DOWN_SX, this.getController(), this.getView());
-        this.inventoryContent.getChildren().add(node.getNode());
-        this.viewControllers.add(node.getController());
-        node = ViewNodeWrapperFactory.createViewNodeWrapper(INIT_NODE_DOWN_DX, this.getController(), this.getView());
-        this.statisticsContent.getChildren().add(node.getNode());
-        this.viewControllers.add(node.getController());
+        this.showNode(gameContent, INIT_NODE_UP, Position.UP);
+        this.showNode(inventoryContent, INIT_NODE_DOWN_SX, Position.DOWN_SX);
+        this.showNode(statisticsContent, INIT_NODE_DOWN_DX, Position.DOWN_DX);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void showTargets(final List<ActionActor> targetables, final List<ActionActor> alliedParty,
+            final List<ActionActor> enemyParty, final Action action) {
+        if (this.getExplorationPaneController().isPresent()) {
+            this.getExplorationPaneController().get().showTargets(targetables, alliedParty, enemyParty, action);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void hideTargets() {
+        if (this.getExplorationPaneController().isPresent()) {
+            this.getExplorationPaneController().get().hideTargets();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void logAction(final ActionResult result) {
+        if (this.getExplorationPaneController().isPresent()) {
+            this.getExplorationPaneController().get().logAction(result);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void visualizeAction(final ActionResult result) {
+        if (this.getExplorationPaneController().isPresent()) {
+            this.getExplorationPaneController().get().visualizeAction(result);
+        }
+    }
+
+    @Override
+    public final void showInventory() {
+        this.showNode(inventoryContent, ViewNode.INVENTORY, Position.DOWN_SX);
+    }
+
+    @Override
+    public final void showActionSelector() {
+        this.showNode(inventoryContent, ViewNode.ACTION_SELECTOR, Position.DOWN_SX);
+    }
+
+    private void showNode(final AnchorPane pane, final ViewNode typeOfNode, final Position pos) {
+        final ViewNodeWrapper node = ViewNodeWrapperFactory.createViewNodeWrapper(typeOfNode, this.getController(),
+                                                                                  this.getView());
+        pane.getChildren().add(node.getNode());
+        this.nodeControllers.put(pos, Optional.of(node.getController()));
+    }
+
+    private Optional<ExplorationView>  getExplorationPaneController() {
+        return this.nodeControllers.values().stream().filter(c -> c.isPresent())
+                                                     .map(c -> c.get())
+                                                     .filter(c -> c instanceof ExplorationView)
+                                                     .map(c -> (ExplorationView) c)
+                                                     .findAny();
     }
 }
