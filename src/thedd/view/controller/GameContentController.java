50ee66edd6ad68ebfbe3a313a953517a29dcd062
Alex Speranza <alex.speranza@studio.unibo.it>
2019-04-19 00:32:22 +0200

    Adds logger to the main pane. Implements showTargets and hideTargets. Adds a utility private method

--- src/thedd/view/controller/GameContentController.java
+++ src/thedd/view/controller/GameContentController.java
@@ -1,12 +1,20 @@
 package thedd.view.controller;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
+import thedd.model.character.BasicCharacter;
+import thedd.model.character.statistics.StatValues;
+import thedd.model.character.statistics.Statistic;
 import thedd.model.combat.action.Action;
 import thedd.model.combat.action.result.ActionResult;
 import thedd.model.combat.actor.ActionActor;
@@ -16,6 +24,8 @@ import thedd.view.explorationpane.ExplorationPaneImpl;
 import thedd.view.explorationpane.TopStackPane;
 import thedd.view.explorationpane.enums.PartyType;
 import thedd.view.explorationpane.enums.TargetSelectionState;
+import thedd.view.explorationpane.logger.LoggerImpl;
+import thedd.view.explorationpane.logger.LoggerManager;
 
 public class GameContentController extends ViewNodeControllerImpl implements Observer<Pair<PartyType, Integer>>, ControllerShowMethods {
 
@@ -23,8 +33,11 @@ public class GameContentController extends ViewNodeControllerImpl implements Obs
     private TopStackPane mainPane;
 
     private final ExplorationPaneImpl explorationPane = new ExplorationPaneImpl();
+    private final LoggerImpl log = new LoggerImpl();
     private TargetSelectionState state;
     private Optional<InteractableActionPerformer> performer = Optional.empty();
+    private final List<ActionActor> alliedParty = new ArrayList<>();
+    private final List<ActionActor> enemyParty = new ArrayList<>();
 
     @Override
     public final void update() {
@@ -38,7 +51,18 @@ public class GameContentController extends ViewNodeControllerImpl implements Obs
         mainPane.setDialogDeclined(() -> cancelInput()); 
         mainPane.getChildren().add(explorationPane);
         explorationPane.changeBackgroundImage(new Image(ClassLoader.getSystemResourceAsStream("bgimg.jpg")));
-        explorationPane.getRoomAdvancer().setOnMouseClicked(e -> mainPane.showDialog("Cambi stanza?"));
+        explorationPane.getRoomAdvancer().setOnMouseClicked(e -> provaLog()/*mainPane.showDialog("Cambi stanza?")*/);
+        explorationPane.autosize();
+
+        final int two = 2;
+        final int six = 6;
+        final int bottomPadding = 10;
+
+        log.setVisible(false);
+        log.getWidthProperty().bind(explorationPane.widthProperty().divide(two));
+        log.getHeightProperty().bind(explorationPane.heightProperty().divide(six));
+        log.translateYProperty().bind(explorationPane.heightProperty().subtract(log.getHeightProperty().add(bottomPadding)).divide(two));
+        mainPane.getChildren().add(log);
     }
 
     @Override
@@ -52,6 +76,8 @@ public class GameContentController extends ViewNodeControllerImpl implements Obs
             case COMBAT_INFORMATION:
                 //prendo il personaggio in posizione x
                 //dico alla view delle statistiche di aggiornarsi con le statistiche per pg selezionato
+                
+                //this.getController().getStatisticsInformations().setCharacter(character);
                 break;
             case COMBAT_TARGET:
                 //prendo il personaggio in posizione x
@@ -60,6 +86,17 @@ public class GameContentController extends ViewNodeControllerImpl implements Obs
             default:
                 break;
         }
+    }
+
+    private void provaLog() {
+        final LinkedList<String> queue = new LinkedList<>();
+        queue.add("Prova1");
+        queue.add("Prova2");
+        final LoggerManager lm = new LoggerManager(log, queue);
+        log.setLoggerManager(lm);
+        final Thread t = new Thread(lm);
+        t.setDaemon(true);
+        t.start();
     }
 
     private void continueInput() {
@@ -75,18 +112,44 @@ public class GameContentController extends ViewNodeControllerImpl implements Obs
                             final List<ActionActor> alliedParty, 
                             final List<ActionActor> enemyParty,
                             final Action action) {
-        // TODO Auto-generated method stub
-        
+        final List<Pair<PartyType, Integer>> allActorPositions = new ArrayList<>();
+        final List<Pair<PartyType, Integer>> targetableActors = new ArrayList<>();
+        IntStream.range(0, alliedParty.size())
+                 .peek(i -> allActorPositions.add(new ImmutablePair<>(PartyType.ALLIED, i)))
+                 .filter(i -> targetables.contains(alliedParty.get(i)))
+                 .forEach(i -> {
+                     final Pair<PartyType, Integer> pos = new ImmutablePair<>(PartyType.ALLIED, i);
+                     targetableActors.add(pos);
+                     updateSingleTarget(alliedParty.get(i), pos, Optional.of(action));
+                 });
+        IntStream.range(0, enemyParty.size())
+                 .peek(i -> allActorPositions.add(new ImmutablePair<>(PartyType.ENEMY, i)))
+                 .filter(i -> targetables.contains(enemyParty.get(i)))
+                 .forEach(i -> {
+                     final Pair<PartyType, Integer> pos = new ImmutablePair<>(PartyType.ENEMY, i);
+                     targetableActors.add(pos);
+                     updateSingleTarget(enemyParty.get(i), pos, Optional.of(action));
+                 });
+        explorationPane.setTargetablePositions(targetableActors, allActorPositions);
+        this.alliedParty.addAll(alliedParty);
+        this.enemyParty.addAll(enemyParty);
+        state = TargetSelectionState.COMBAT_TARGET;
     }
 
     @Override
     public void hideTargets() {
-        //mostra tutti come targettablili
+        explorationPane.setAllAsTargetable();
+        state = TargetSelectionState.COMBAT_INFORMATION;
+        IntStream.range(0, alliedParty.size())
+                 .forEach(i -> updateSingleTarget(alliedParty.get(i), new ImmutablePair<>(PartyType.ALLIED, i),  Optional.empty()));
+        IntStream.range(0, enemyParty.size())
+                 .forEach(i -> updateSingleTarget(enemyParty.get(i), new ImmutablePair<>(PartyType.ENEMY, i),  Optional.empty()));
+        alliedParty.clear();
+        enemyParty.clear();
     }
 
     @Override
     public void logAction(final ActionResult result) {
-        // TODO Auto-generated method stub
         
     }
 
@@ -108,5 +171,15 @@ public class GameContentController extends ViewNodeControllerImpl implements Obs
         
     }
 
+    private void updateSingleTarget(final ActionActor target, final Pair<PartyType, Integer> position, final Optional<Action> action) {
+        if (Objects.requireNonNull(target) instanceof BasicCharacter) {
+            final BasicCharacter bcTarget = (BasicCharacter) target;
+            final StatValues targetHP = bcTarget.getStat(Statistic.HEALTH_POINT);
+            final String tooltip = bcTarget.getName() + '\n'
+                                   + "HP: " + targetHP.getActual() + "/" + targetHP.getMax() + '\n'
+                                   + (action.isPresent() ? "Chanche to hit: " + Objects.requireNonNull(action).get().getHitChance(target) + '\n' : "");
+            explorationPane.updatePositionTooltip(Objects.requireNonNull(position), tooltip);
+        }
+    }
 
 }
