7f6157da876173a6a8931e0bed8ca425cd4defe6
Alessandro Lombardini <alessandr.lombardin3@studio.unibo.it>
2019-04-18 17:33:19 +0200

    Now Environment set first floor alone and return boolean if you try to set next floor instead of launch an exeption

--- src/thedd/model/world/environment/Environment.java
+++ src/thedd/model/world/environment/Environment.java
@@ -14,13 +14,10 @@ public interface Environment {
      * This method allows to set the next floor.
      * 
      * @param floorDetails is the details of the floor that has to be setted
+     * @return true is the floor has been setted
      * @throws NullPointerExeption     if floorDetails is null
-     * @throws IllegalStateException   if the current floor isn't completed
-     * @throws IllegalArgumentExeption if floorDetails doesn't own to the list of
-     *                                 FloorDetails given by the method
-     *                                 getFloorOptions().
      */
-    void setNextFloor(FloorDetails floorDetails);
+    boolean setNextFloor(FloorDetails floorDetails);
 
     /**
      * This method allows to get the current floor.
@@ -33,6 +30,7 @@ public interface Environment {
     /**
      * This method allows to get the current floor index. If the index value is
      * NONE_FLOORS constant (-1) it means there aren't floor yet.
+     * The first floor is 0, the ground floor.
      * 
      * @return the current floor index
      */
