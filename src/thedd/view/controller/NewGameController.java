dbace321477f1fda4abfb9775f6e73dff804dc06
Alessandro Lombardini <alessandr.lombardin3@studio.unibo.it>
2019-04-18 19:51:20 +0200

    Add title image to newGame scene

--- src/thedd/view/controller/NewGameController.java
+++ src/thedd/view/controller/NewGameController.java
@@ -1,7 +1,15 @@
 package thedd.view.controller;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import thedd.view.ApplicationViewState;
 import thedd.view.extensions.AdaptiveFontLabel;
 
@@ -13,6 +21,10 @@ public class NewGameController extends ViewNodeControllerImpl {
     private static final String ERROR_TITLE_INPUTERROR = "Input error";
     private static final String ERROR_TEXT_NULL = "Nope, you have to insert number of rooms and floors";
     private static final String ERROR_TEXT_NONVALIDVALUE = "Nope, the number of rooms and/or floors is not acceptable";
+    private static final String NEW_GAME_TITLE_URL = "images" + System.getProperty("file.separator") + "titles"
+                                                      + System.getProperty("file.separator") + "gameOver_title.png";
+    private static final double TITLE_HEIGHT_PERC = 1.0;
+    private static final double TITLE_WIDTH_PERC = 1.0;
 
     @FXML
     private TextField playerNameTextField;
@@ -31,6 +43,9 @@ public class NewGameController extends ViewNodeControllerImpl {
 
     @FXML
     private AdaptiveFontLabel numberOfFloorsLabel;
+
+    @FXML
+    private AnchorPane newGameTitleImage;
 
 
     /**
@@ -62,6 +77,14 @@ public class NewGameController extends ViewNodeControllerImpl {
      */
     @Override
     protected void initView() {
-        /* Has nothing to init */
+        final Image imageNewGame = new Image(NEW_GAME_TITLE_URL);
+        final BackgroundImage backgroundGameOver = new BackgroundImage(imageNewGame, BackgroundRepeat.NO_REPEAT, 
+                                                               BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, 
+                                                               new BackgroundSize(TITLE_HEIGHT_PERC, TITLE_WIDTH_PERC, 
+                                                               true, true, true, false));
+        this.newGameTitleImage.setBackground(new Background(backgroundGameOver));
+        this.playerNameLabel.setAlignment(Pos.CENTER_RIGHT);
+        this.numberOfRoomsLabel.setAlignment(Pos.CENTER_RIGHT);
+        this.numberOfFloorsLabel.setAlignment(Pos.CENTER_RIGHT);
     }
 }
