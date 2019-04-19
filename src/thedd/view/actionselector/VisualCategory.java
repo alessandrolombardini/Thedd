21b23a294d0135528a1009bfa83f42a4492086ab
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-04-19 20:50:12 +0200

    VisualCategory class: change image loading to use new ImageLoaderImpl class, change getDescription method to return a formatted text complete with all information regarding the action category

--- src/thedd/view/actionselector/VisualCategory.java
+++ src/thedd/view/actionselector/VisualCategory.java
@@ -6,36 +6,68 @@ import java.util.Collections;
 import java.util.List;
 
 import javafx.scene.image.Image;
+import thedd.model.combat.action.Action;
+import thedd.view.imageloader.DirectoryPicker;
+import thedd.view.imageloader.ImageLoaderImpl;
 
+/**
+ * A class used to display information about the category of a group of {@link Action}s.<br>
+ * It holds an image associated with the action and specifies the formatting
+ * of the text.
+ */
 public class VisualCategory {
 
     private final String name;
     private final List<VisualAction> actions = new ArrayList<>();
     private final Image image;
 
+    /**
+     * @param name the name of the category
+     * @param actions the list of associated actions
+     */
     public VisualCategory(final String name, final List<VisualAction> actions) {
         this.name = name;
         this.actions.addAll(actions);
-        final String fileName = name.toLowerCase() + ".png";
-        final URL url = this.getClass().getResource("/actions/categories/" + fileName);
-        if (url != null) {
-            this.image = new Image(url.toString());
-        } else {
-            System.err.println("Image for category " + fileName + " not found");
-            this.image = new Image(this.getClass().getResourceAsStream("/actions/not_found.png"));
-        }
+        this.image = new ImageLoaderImpl().loadSingleImage(DirectoryPicker.ACTION_CATEGORIES, name);
     }
 
+    /**
+     * Gets the name of the category.
+     * @return the name of the category
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the list of {@link VisualAction}s associated with the category.
+     * @return the list of actions link to the category
+     */
     public List<VisualAction> getActions() {
         return Collections.unmodifiableList(actions);
     }
 
+    /**
+     * Gets an image representing the category.
+     * @return the image linked to the category
+     */
     public Image getImage() {
         return image;
+    }
+
+    /**
+     * Gets a text describing the category to be displayed to the player.
+     * @return the description of the category
+     */
+    public String getDescription() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("Category name: ");
+        sb.append(getName());
+        if (!getActions().isEmpty()) {
+            sb.append("\n\nActions in this category: ");
+            getActions().forEach(a -> sb.append("\n\t").append(a.getName()));
+        }
+        return sb.toString();
     }
 
 }
