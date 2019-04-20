b860d65bab56d48470b5e41b79292e32281ec4e7
Alessandro Lombardini <alessandr.lombardin3@studio.unibo.it>
2019-04-21 00:11:07 +0200

    Update of javadoc of Enviroment and change isLastFloor()

--- src/thedd/model/world/environment/Environment.java
+++ src/thedd/model/world/environment/Environment.java
@@ -14,8 +14,9 @@ public interface Environment {
      * This method allows to set the next floor.
      * 
      * @param floorDetails is the details of the floor that has to be setted
-     * @return true is the floor has been setted
-     * @throws NullPointerExeption     if floorDetails is null
+     * @return false if FloorDetails is not one of FloorDetails given by this class
+     *         to change the current floor or the current floor is not changeable.
+     * @throws NullPointerExeption if floorDetails is null
      */
     boolean setNextFloor(FloorDetails floorDetails);
 
@@ -29,7 +30,7 @@ public interface Environment {
 
     /**
      * This method allows to get the current floor index. If the index value is
-     * NONE_FLOORS constant (-1) it means there aren't floor yet.
+     * below 0 it means there aren't floor yet.
      * The first floor is 0, the ground floor.
      * 
      * @return the current floor index
@@ -44,7 +45,7 @@ public interface Environment {
     boolean isCurrentLastFloor();
 
     /**
-     * This method allows to get a list of possible next floors.
+     * This method allows to get a list of possible next floors like FloorDetails.
      * 
      * @return the list of possible FloorDetails
      * @throws IllegalStateException if this is the last floor, it means that floors
