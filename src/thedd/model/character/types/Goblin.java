531b4ffb39d8b0dcd496c8152a5e26df6e9c9f9e
Alex Speranza <alex.speranza@studio.unibo.it>
2019-04-20 20:45:04 +0200

    Adds a base action

--- src/thedd/model/character/types/Goblin.java
+++ src/thedd/model/character/types/Goblin.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import thedd.model.character.BasicCharacterImpl;
+import thedd.model.combat.action.TargetType;
 import thedd.model.combat.action.effect.ActionEffect;
+import thedd.model.combat.action.implementations.LightAttack;
 import thedd.model.combat.modifier.DamageModifier;
 import thedd.model.combat.modifier.Modifier;
 import thedd.model.combat.modifier.ModifierActivation;
@@ -12,6 +14,7 @@ import thedd.model.combat.requirements.tags.TagRequirement;
 import thedd.model.combat.requirements.tags.TagRequirementType;
 import thedd.model.combat.tag.EffectTag;
 import thedd.model.combat.tag.Tag;
+import thedd.utils.randomcollections.RandomPrority;
 
 /**
  * Goblin extension of {@link thedd.model.character.BasicCharacterImpl}.
@@ -27,7 +30,7 @@ public class Goblin extends BasicCharacterImpl {
     public Goblin(final String name, final double multiplier) {
         super(name, multiplier);
         setPermanentModifiers();
-        // this.addWeightedAction(new ActionImpl() , RandomActionPrority.DEFAULT);
+        this.addWeightedAction(new LightAttack(TargetType.FOE), RandomPrority.DEFAULT);
     }
 
     private void setPermanentModifiers() {
--- src/thedd/model/character/types/Headless.java
+++ src/thedd/model/character/types/Headless.java
@@ -1,6 +1,9 @@
 package thedd.model.character.types;
 
 import thedd.model.character.BasicCharacterImpl;
+import thedd.model.combat.action.TargetType;
+import thedd.model.combat.action.implementations.LightAttack;
+import thedd.utils.randomcollections.RandomPrority;
 
 /**
  * Headless extension of {@link thedd.model.character.BasicCharacterImpl}.
@@ -16,5 +19,7 @@ public class Headless extends BasicCharacterImpl {
     public Headless(final String name, final double multiplier) {
         super(name, multiplier);
         // ret.addWeightedAction(new ActionImpl() , RandomActionPrority.DEFAULT);
+        this.addWeightedAction(new LightAttack(TargetType.FOE), RandomPrority.DEFAULT);
+
     }
 }
