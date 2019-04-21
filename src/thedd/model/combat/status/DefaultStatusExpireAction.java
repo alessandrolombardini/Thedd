21881b167bf1842522073d60314da09e61d68baa
Ivan Perazzini <ivan.perazzini@gmail.com>
2019-04-21 11:58:06 +0200

    Add default action for status expiration

---
+++ src/thedd/model/combat/status/DefaultStatusExpireAction.java
@@ -0,0 +1,28 @@
+package thedd.model.combat.status;
+
+import thedd.model.combat.action.ActionBuilder;
+import thedd.model.combat.action.ActionCategory;
+import thedd.model.combat.action.ActionImpl;
+import thedd.model.combat.action.LogMessageType;
+import thedd.model.combat.action.TargetType;
+
+/** 
+ * Default action that signals when a status is expired.
+ */
+public class DefaultStatusExpireAction extends ActionImpl {
+
+    private static final double BASE_HITCHANCE = 1d;
+
+    /**
+     * @param statusName, the name of the expired status
+     */
+    public DefaultStatusExpireAction(final String statusName) {
+        super(new ActionBuilder().setName(statusName)
+                                 .setBaseHitChance(BASE_HITCHANCE)
+                                 .setCategory(ActionCategory.STATUS)
+                                 .setTargetType(TargetType.SELF)
+                                 .setLogMessage(LogMessageType.STATUS_EXPIRE)
+                                 .build());
+    }
+
+}
