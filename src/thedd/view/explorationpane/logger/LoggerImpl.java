67a814c288e043a8b3548b352bd1e2e3217183d2
Alex Speranza <alex.speranza@studio.unibo.it>
2019-04-22 01:12:12 +0200

    Removes close button as it is no longer needed

--- src/thedd/view/explorationpane/logger/LoggerImpl.java
+++ src/thedd/view/explorationpane/logger/LoggerImpl.java
@@ -1,43 +1,34 @@
 package thedd.view.explorationpane.logger;
 
 import java.util.Objects;
-import java.util.Optional;
 
 import javafx.animation.FadeTransition;
 import javafx.beans.property.DoubleProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
 import thedd.view.extensions.AdaptiveFontLabel;
-import thedd.view.imageloader.DirectoryPicker;
-import thedd.view.imageloader.ImageLoader;
-import thedd.view.imageloader.ImageLoaderImpl;
 
 /**
  * {@link GridPane} which acts as {@link Logger}.
  */
 public class LoggerImpl extends GridPane implements Logger {
 
-    private static final int FONT_RATIO = 25;
+    private static final int FONT_RATIO = 30;
     private static final Duration FADE_DURATION = Duration.millis(500);
     private static final double TEXT_WIDTH_PERC = 90;
     private static final double BTN_WIDTH_PERC = 10;
     private static final double ROW_HEIGHT_PERC = 50;
     private static final double PADDING = 5;
 
-    private final AdaptiveFontLabel text;
-    private Optional<LoggerManager> loggerManager;
+    private final AdaptiveFontLabel text = new AdaptiveFontLabel(FONT_RATIO);
+    //private Optional<LoggerManager> loggerManager;
 
     /**
      * 
@@ -58,16 +49,16 @@ public class LoggerImpl extends GridPane implements Logger {
         this.setPadding(new Insets(PADDING));
         this.setBackground(new Background(new BackgroundFill(Color.web("#040404"), null, null)));
 
-        text = new AdaptiveFontLabel(FONT_RATIO);
         text.setTextFill(Color.WHITE);
+        text.setWrapText(true);
         text.prefWidthProperty().bind(this.getWidthProperty().multiply(TEXT_WIDTH_PERC / 100));
         text.prefHeightProperty().bind(this.getHeightProperty());
         text.setAlignment(Pos.TOP_LEFT);
-        this.add(text, 0, 0, 1, 1);
+        this.add(text, 0, 0, 2, 2);
 
-        loggerManager = Optional.empty();
+        //loggerManager = Optional.empty();
 
-        final ImageLoader imgl = new ImageLoaderImpl();
+        /*final ImageLoader imgl = new ImageLoaderImpl();
         final Button close = new Button();
         close.setPickOnBounds(true);
         close.setTranslateY(PADDING);
@@ -77,7 +68,7 @@ public class LoggerImpl extends GridPane implements Logger {
                               new BackgroundImage(
                                 imgl.loadSingleImage(DirectoryPicker.ICON, "closelogger"), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(1.0,  1.0,  true, true, true, false))));
         close.setOnAction(e -> loggerManager.ifPresent(lm -> lm.cancel()));
-        this.add(close, 1, 0, 1, 1);
+        this.add(close, 1, 0, 1, 1);*/
     }
 
     @Override
@@ -100,7 +91,7 @@ public class LoggerImpl extends GridPane implements Logger {
 
     @Override
     public final void setLoggerManager(final LoggerManager logMan) {
-        loggerManager = Optional.of(Objects.requireNonNull(logMan));
+        //loggerManager = Optional.of(Objects.requireNonNull(logMan));
     }
 
     @Override
