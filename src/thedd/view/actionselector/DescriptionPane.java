alessandrolombardini/Thedd

commit fa87ed30f9790af41be58f45a8b286bab98e3d58

Fix pdm and checkstyle errors

src/thedd/view/actionselector/DescriptionPane.java
@@ -43,6 +43,7 @@ public class DescriptionPane extends Pane implements Observable<Command> {
      * Public constructor, sets up the structure of the pane.
      */
     public DescriptionPane() {
+        super();
         upperAreaHeight.bind(this.heightProperty().multiply(UPPER_AREA_HEIGHT_PERC));
         lowerAreaHeight.bind(this.heightProperty().subtract(upperAreaHeight));
         padding.bind(this.widthProperty().multiply(PADDING_PERC));
@@ -172,10 +173,11 @@ public class DescriptionPane extends Pane implements Observable<Command> {
             } else {
                 //Code taken and modified from https://stackoverflow.com/questions/33157671/javafx-how-to-repeat-action-as-long-as-button-is-pressed
                 final AnimationTimer timer = new AnimationTimer() {
-                    private long lastUpdate = 0;
+                    private long lastUpdate;
 
                     @Override
                     public void handle(final long time) {
+                        lastUpdate = 0;
                         final long timeStampMs = time / 1000000;
                         if (Math.abs(timeStampMs - lastUpdate) >= PRESS_INTERVAL_MS) {
                             currentCommand = onClickCommand;

src/test/EnvironmentTest.java
@@ -18,7 +18,7 @@ import thedd.model.world.environment.EnvironmentImpl;
 import thedd.model.world.floor.Floor;
 import thedd.model.world.floor.FloorDetailsFactory;
 import thedd.model.world.floor.FloorDetailsFactoryImpl;
-import thedd.model.world.floor.FloorDetails.FloorDetails;
+import thedd.model.world.floor.details.FloorDetails;
 import thedd.model.world.room.Room;
 import thedd.model.world.room.RoomFactory;
 import thedd.model.world.room.RoomFactoryImpl;
@@ -28,14 +28,14 @@ import thedd.model.world.room.RoomFactoryImpl;
  */
 public class EnvironmentTest {
 
-    private final int numberOfTest = 100;
+    private static final int NOMBER_OF_TEST = 100;
 
     /**
      * Test of new environment.
      */
     @Test
     public void testCreateEnvironement() {
-        for (int i = 1; i < numberOfTest; i++) {
+        for (int i = 1; i < NOMBER_OF_TEST; i++) {
             final int numberOfRooms = i + i;
             final int numberOfFloors = i;
             final Environment environment = new EnvironmentImpl(numberOfFloors, numberOfRooms);
@@ -46,7 +46,6 @@ public class EnvironmentTest {
             assertEquals(floor.getCurrentRoomIndex(), 0);
             assertFalse(floor.checkToChangeFloor());
             assertTrue(floor.hasNextRoom());
-            final Room room = floor.getCurrentRoom();
         }
     }
 
@@ -66,7 +65,7 @@ public class EnvironmentTest {
     private void testWithDifferentDifficulty(final Difficulty difficulty, final boolean boss) {
         final FloorDetailsFactory factory = new FloorDetailsFactoryImpl();
         final int roomForBoss = boss ? 1 : 0;
-        for (int i = 1; i < numberOfTest; i++) {
+        for (int i = 1; i < NOMBER_OF_TEST; i++) {
             final FloorDetails details = factory.createFloorDetails(difficulty, i, boss);
             assertEquals(details.isBossFloor(), boss);
             assertTrue(details.getNumberOfEnemies() <= RoomFactoryImpl.MAX_ENEMIES_PER_ROOM * (i - roomForBoss));
@@ -127,7 +126,7 @@ public class EnvironmentTest {
 
     private void testRoomFactoryByDifficulty(final Difficulty diff) {
         final FloorDetailsFactory factory = new FloorDetailsFactoryImpl();
-        for (int i = EnvironmentImpl.MIN_NUMBER_OF_ROOMS; i < numberOfTest; i++) {
+        for (int i = EnvironmentImpl.MIN_NUMBER_OF_ROOMS; i < NOMBER_OF_TEST; i++) {
             final FloorDetails details = factory.createFloorDetails(diff, i, false);
             final RoomFactory roomFactory = new RoomFactoryImpl(details);
             int numberOfEnemies = 0;

src/test/ItemsTest.java
@@ -1,14 +1,12 @@
 package test;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-
 import java.util.Optional;
-
 import org.junit.Test;
-
 import thedd.model.character.CharacterFactory;
 import thedd.model.item.ItemRarityImpl;
 import thedd.model.item.equipableitem.EquipableItem;
@@ -61,7 +59,7 @@ public class ItemsTest {
     public void testEqualsUsableItems() {
         final UsableItem usable1 = new UsableItemImpl(0, NAME, ItemRarityImpl.COMMON, DESCRIPTION, false, false);
         final UsableItem usable2 = new UsableItemImpl(0, NAME, ItemRarityImpl.COMMON, DESCRIPTION, false, false);
-        assertTrue(usable1.equals(usable2));
+        assertEquals(usable1, usable2);
     }
 
     /**
@@ -73,8 +71,8 @@ public class ItemsTest {
                 DESCRIPTION);
         final EquipableItem equipable2 = new EquipableItemImpl(0, NAME, EquipableItemType.AMULET, ItemRarityImpl.COMMON,
                 DESCRIPTION);
-        assertTrue(equipable1.equals(equipable2));
+        assertEquals(equipable1, equipable2);
         equipable2.onEquip(CharacterFactory.createPlayerCharacter(Optional.empty(), Difficulty.EASY));
-        assertFalse(equipable1.equals(equipable2));
+        assertNotEquals(equipable1, equipable2);
     }
 }

src/test/RoomEventTest.java
@@ -1,15 +1,11 @@
 package test;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.Test;
-
 import thedd.model.character.CharacterFactory;
-import thedd.model.character.types.EnemyCharacterType;
 import thedd.model.roomevent.RoomEventType;
 import thedd.model.roomevent.combatevent.CombatEvent;
 import thedd.model.roomevent.combatevent.CombatEventImpl;

src/thedd/controller/Controller.java
@@ -9,7 +9,7 @@ import thedd.model.combat.actor.ActionActor;
 import thedd.model.character.BasicCharacter;
 import thedd.model.item.Item;
 import thedd.model.roomevent.RoomEvent;
-import thedd.model.world.floor.FloorDetails.FloorDetails;
+import thedd.model.world.floor.details.FloorDetails;
 
 /**
  * Interface describing the controller of the pattern MVC of this application.

src/thedd/controller/ControllerImpl.java
@@ -4,8 +4,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.IntStream;
-
 import javafx.application.Platform;
 import thedd.controller.information.PlayerInformation;
 import thedd.controller.information.PlayerInformationImpl;
@@ -26,13 +24,12 @@ import thedd.model.combat.instance.ActionExecutionInstance;
 import thedd.model.combat.instance.CombatStatus;
 import thedd.model.combat.instance.ExecutionInstanceImpl;
 import thedd.model.item.Item;
-import thedd.model.item.ItemFactory;
 import thedd.model.item.usableitem.UsableItem;
 import thedd.model.roomevent.RoomEvent;
 import thedd.model.roomevent.RoomEventType;
 import thedd.model.roomevent.combatevent.CombatEvent;
 import thedd.model.world.environment.EnvironmentImpl;
-import thedd.model.world.floor.FloorDetails.FloorDetails;
+import thedd.model.world.floor.details.FloorDetails;
 import thedd.model.world.room.Room;
 import thedd.view.ApplicationViewState;
 import thedd.view.View;
@@ -42,6 +39,8 @@ import thedd.view.View;
  */
 public class ControllerImpl implements Controller {
 
+    private static final String SELECT_ACTION = "Select an action";
+    private static final String SELECT_TARGET = "Select a target";
     private final View view;
     private final Model model;
     private PlayerInformation playerInfo;
@@ -70,21 +69,14 @@ public class ControllerImpl implements Controller {
             this.model.initGame(Optional.ofNullable(playerName), numOfFloors, numOfRooms);
             this.playerInfo = new PlayerInformationImpl(this.model.getPlayerCharacter());
             this.statisticsInfo = new StatisticsInformationImpl(this.model.getPlayerCharacter());
-
-//---------------------QUESTO A LAVORI ULTIMATI è DA RIMUOVERE.
-            BasicCharacter charac = this.model.getPlayerCharacter();
-            IntStream.range(0, 15).forEach(i -> {
-                Item ite = ItemFactory.getRandomItem();
-                if (ite.isUsable())
-                    charac.getInventory().addItem(ite);
-            });
-//------------------------------------------------------------
-            //return nextRoom();
             return true;
         }
         return false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final boolean isValidNumberOfRooms(final String numberOfRooms) {
         if (this.checkNumber(numberOfRooms)) {
@@ -94,6 +86,9 @@ public class ControllerImpl implements Controller {
         return false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final boolean isValidNumberOfFloors(final String numberOfFloors) {
         if (this.checkNumber(numberOfFloors)) {
@@ -115,7 +110,6 @@ public class ControllerImpl implements Controller {
         Platform.exit();
     }
 
-    // -------------------------------------------------------------------
     /**
      * {@inheritDoc}
      */
@@ -141,7 +135,6 @@ public class ControllerImpl implements Controller {
         if (item.isUsable()) {
             final UsableItem usable = (UsableItem) item;
             playerInfo.setUsedItem(item);
-//            this.view.update();
             final Action itemAction = usable.getAction();
             itemAction.setSource(getPlayer());
             if (getPlayer().isInCombat()) {
@@ -193,12 +186,8 @@ public class ControllerImpl implements Controller {
     @Override
     public void updateStatistics(final BasicCharacter character) {
         this.statisticsInfo.setCharacter(character);
-        //this.view.update();
         view.partialUpdate();
     }
-
-    // ----------------------------------------------------Action execution and
-    // combat management session
 
     /**
      * {@inheritDoc}
@@ -209,7 +198,7 @@ public class ControllerImpl implements Controller {
         playerActor.resetSelectedAction();
         playerInfo.resetUsedItem();
         if (isCombatActive()) {
-            view.showMessage("Select an action");
+            view.showMessage(SELECT_ACTION);
         } else {
             view.hideMessage();
         }
@@ -221,9 +210,6 @@ public class ControllerImpl implements Controller {
      */
     @Override
     public void targetSelected(final ActionActor target) {
-        view.disableInteraction();
-
-
         view.hideMessage();
         final ActionActor playerActor = this.model.getPlayerCharacter();
         final ActionExecutor currentExecutor = actionExecutor.get();
@@ -238,9 +224,6 @@ public class ControllerImpl implements Controller {
         if (currentExecutor.isRoundReady()) {
             evaluateNextAction();
         }
-
-
-        view.enableInteraction();
     }
 
     /**
@@ -254,9 +237,8 @@ public class ControllerImpl implements Controller {
         instance.addPlayerPartyMember(playerActor);
         actionExecutor.get().setExecutionInstance(instance);
         if (action.getTargets().isEmpty()) {
-            // call view to tell player to select a target [DONE]
             model.getPlayerCharacter().addActionToQueue(action, true);
-            view.showMessage("Select a target");
+            view.showMessage(SELECT_TARGET);
             final List<ActionActor> targetables = action.getValidTargets(instance);
             view.showActionTargets(targetables, instance.getPlayerParty(), instance.getNPCsParty(), action);
         } else {
@@ -309,12 +291,12 @@ public class ControllerImpl implements Controller {
             evaluateNextAction();
             break;
         case ROUND_PAUSED:
-            view.showMessage("Select an action");
+            view.showMessage(SELECT_ACTION);
             view.showActionSelector();
             view.update();
             break;
         case ROUND_ENDED:
-            view.showMessage("Select an action");
+            view.showMessage(SELECT_ACTION);
             view.showActionSelector();
             executor.prepareNextRound();
             view.update();
@@ -336,9 +318,8 @@ public class ControllerImpl implements Controller {
         if (combatExecutor.isRoundReady()) {
             evaluateNextAction();
         } else {
-            // call view to tell player to select an action
             view.showActionSelector();
-            view.showMessage("Select an action");
+            view.showMessage(SELECT_ACTION);
         }
     }
 
@@ -370,18 +351,19 @@ public class ControllerImpl implements Controller {
     @Override
     public void selectAction(final Action action) {
         model.getPlayerCharacter().addActionToQueue(action, true);
-        // call view to tell player to select a target (even if the action target type
-        // is SELF?)
         final ActionExecutionInstance aei = actionExecutor.get().getExecutionInstance();
         if (action.getTargetType() == TargetType.SELF) {
             targetSelected(action.getSource().get());
         } else {
             final List<ActionActor> targetables = action.getValidTargets(aei);
             view.showActionTargets(targetables, aei.getPlayerParty(), aei.getNPCsParty(), action);
-            view.showMessage("Select a target");
+            view.showMessage(SELECT_TARGET);
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final boolean nextRoom() {
         final boolean isChanged = this.model.getEnvironment().getCurrentFloor().nextRoom();
@@ -401,11 +383,17 @@ public class ControllerImpl implements Controller {
         return isChanged;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final boolean nextFloor(final FloorDetails floorDetails) {
         return this.model.getEnvironment().setNextFloor(floorDetails);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final List<RoomEvent> getRoomEvents() {
         if (this.model.getEnvironment().getCurrentFloor().getCurrentRoomIndex() >= 0) {
@@ -414,31 +402,49 @@ public class ControllerImpl implements Controller {
         return Collections.emptyList();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final List<FloorDetails> getStairsOptions() {
         return this.model.getEnvironment().getFloorOptions();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final boolean isCurrentLastFloor() {
         return this.model.getEnvironment().isCurrentLastFloor();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final boolean isCurrentLastRoom() {
         return !this.model.getEnvironment().getCurrentFloor().hasNextRoom();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final boolean isCurrentRoomCompleted() {
         return this.model.getEnvironment().getCurrentFloor().getCurrentRoom().checkToMoveOn();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final boolean hasPlayerWon() {
         return this.model.hasPlayerWon();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final BasicCharacter getPlayer() {
         return this.model.getPlayerCharacter();

src/thedd/model/combat/action/ActionBuilder.java
@@ -28,7 +28,7 @@ import thedd.model.combat.actor.ActionActor;
  */
 public class ActionBuilder {
 
-    private ActionActor source = null;
+    private ActionActor source;
     private String name = "[Name missing]";
     private String description = "[Description missing]";
     private List<ActionEffect> effects = new ArrayList<>();
@@ -37,7 +37,7 @@ public class ActionBuilder {
     private ActionTargeting targeting = new DefaultTargeting();
     private ExecutionPolicy applyPolicy = new DefaultExecutionPolicy();
     private TargetType targetType = TargetType.EVERYONE;
-    private double baseHitChance = 0d;
+    private double baseHitChance;
 
     /**
      * Sets the target type.
@@ -45,6 +45,8 @@ public class ActionBuilder {
      * @return the updated builder
      */
     public ActionBuilder setTargetType(final TargetType targetType) {
+        source = null;
+        baseHitChance = 0d;
         this.targetType = targetType;
         return this;
     }

src/thedd/model/combat/action/executionpolicies/ExtraActionToSource.java
@@ -19,6 +19,7 @@ public class ExtraActionToSource extends DefaultExecutionPolicy {
      * @param extraAction the extra action to assign to the source actor
      */
     public ExtraActionToSource(final Action extraAction) {
+        super();
         this.extraAction = extraAction;
     }
 

commit db1debee0282a185a36b556698eb854bc4318bec

DescriptionPane class: fix error in handle method introduced in fa87ed3

src/thedd/view/actionselector/DescriptionPane.java
@@ -177,7 +177,6 @@ public class DescriptionPane extends Pane implements Observable<Command> {
 
                     @Override
                     public void handle(final long time) {
-                        lastUpdate = 0;
                         final long timeStampMs = time / 1000000;
                         if (Math.abs(timeStampMs - lastUpdate) >= PRESS_INTERVAL_MS) {
                             currentCommand = onClickCommand;